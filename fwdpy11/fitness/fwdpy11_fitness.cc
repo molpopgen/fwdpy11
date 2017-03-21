c57a942e372fdaa258fc7e5da16a27e846f622dc
molpopgen <krthornt@uci.edu>
2017-03-21 13:19:53 -0700

    cleaner wrapping of fwdpp's fitness types

--- fwdpy11/fitness/fwdpy11_fitness.cc
+++ fwdpy11/fitness/fwdpy11_fitness.cc
@@ -9,42 +9,46 @@ using singlepop_fitness = std::function<double(const fwdpy11::diploid_t &,
                                                const fwdpy11::gcont_t &,
                                                const fwdpy11::mcont_t &)>;
 
-template <typename fitness_model> struct fwdpp_singlepop_fitness_wrapper
+template <typename fitness_model_type> struct fwdpp_singlepop_fitness_wrapper
 {
-    fitness_model f;
+    using fitness_model = fitness_model_type;
     const double scaling;
-    fwdpp_singlepop_fitness_wrapper(const double scaling_)
-        : f(fitness_model()), scaling(scaling_)
+    fwdpp_singlepop_fitness_wrapper(const double scaling_) : scaling(scaling_)
     {
     }
-    inline double
-    operator()(const fwdpy11::diploid_t &dip, const fwdpy11::gcont_t &gametes,
-               const fwdpy11::mcont_t &mutations)
+    inline singlepop_fitness
+    callback() const
     {
-        return f(dip, gametes, mutations, scaling);
+        return std::bind(fitness_model(), std::placeholders::_1,
+                         std::placeholders::_2, std::placeholders::_3,
+                         scaling);
     }
 };
+
+using singlepop_mult_wrapper
+    = fwdpp_singlepop_fitness_wrapper<KTfwd::multiplicative_diploid>;
+using singlepop_additive_wrapper
+    = fwdpp_singlepop_fitness_wrapper<KTfwd::additive_diploid>;
 
 PYBIND11_PLUGIN(fwdpy11_fitness)
 {
     py::module m("fwdpy11_fitness", "Standard fitness models.");
 
-    py::class_<fwdpp_singlepop_fitness_wrapper<KTfwd::multiplicative_diploid>>(
-        m, "SpopMult")
+    py::class_<singlepop_mult_wrapper>(m, "SpopMult")
         .def(py::init<double>())
         .def_property_readonly(
             "callback",
-            [](const fwdpp_singlepop_fitness_wrapper<KTfwd::
-                                                         multiplicative_diploid>
-                   &m) -> singlepop_fitness { return m; });
+            [](const singlepop_mult_wrapper &m) -> singlepop_fitness {
+                return m.callback();
+            });
 
-    py::class_<fwdpp_singlepop_fitness_wrapper<KTfwd::additive_diploid>>(
-        m, "SpopAdditive")
+    py::class_<singlepop_additive_wrapper>(m, "SpopAdditive")
         .def(py::init<double>())
         .def_property_readonly(
             "callback",
-            [](const fwdpp_singlepop_fitness_wrapper<KTfwd::additive_diploid> &m)
-                -> singlepop_fitness { return m; });
+            [](const singlepop_additive_wrapper &m) -> singlepop_fitness {
+                return m.callback();
+            });
 
     return m.ptr();
 }
