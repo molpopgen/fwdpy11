0638ff5d3a499e2af1b0edef1f465b04c23f68e2
molpopgen <krthornt@uci.edu>
2017-03-20 18:48:35 -0700

    wrap KTfwd::data_matrix

--- fwdpy11/src/fwdpy11_sampling.cc
+++ fwdpy11/src/fwdpy11_sampling.cc
@@ -3,6 +3,7 @@
 #include <pybind11/stl.h>
 #include <fwdpp/forward_types.hpp>
 #include <fwdpp/sugar/sampling.hpp>
+#include <fwdpp/sugar/matrix.hpp>
 #include "types.hpp"
 
 namespace py = pybind11;
@@ -16,10 +17,24 @@ sep_sample_t sample_singlepop(const fwdpy::GSLrng_t & rng, const fwdpy::singlepo
     return KTfwd::sample_separate(rng.get(),pop,nsam,removeFixed);
 }
 
+
+
 PYBIND11_PLUGIN(fwdpy11_sampling) {
     py::module m("fwdpy11_sampling", "Taking samples from populations");
 
     m.def("sample_singlepop",&sample_singlepop);
+
+    py::class_<KTfwd::data_matrix>(m,"DataMatrix")
+        .def(py::init<>())
+        .def(py::init<std::size_t>())
+        .def_readonly("neutral",&KTfwd::data_matrix::neutral)
+        .def_readonly("selected",&KTfwd::data_matrix::selected)
+        .def_readonly("neutral_positions",&KTfwd::data_matrix::neutral_positions)
+        .def_readonly("selected_positions",&KTfwd::data_matrix::selected_positions)
+        .def_readonly("neutral_popfreq",&KTfwd::data_matrix::neutral_popfreq)
+        .def_readonly("selected_popfreq",&KTfwd::data_matrix::selected_popfreq)
+        .def_readonly("nrow",&KTfwd::data_matrix::nrow)
+        ;
 
     return m.ptr();
 }
