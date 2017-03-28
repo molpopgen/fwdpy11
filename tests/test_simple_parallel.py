07bcd3bffd40a92611b2d42c3233b2a4d73b51c3
molpopgen <krthornt@uci.edu>
2017-03-28 15:23:38 -0700

    manual will display some of the integration tests as examples

---
+++ tests/test_simple_parallel.py
@@ -0,0 +1,54 @@
+import fwdpy11 as fp11
+import fwdpy11.wright_fisher as wf
+#concurrent.futures is Python 3 only
+import concurrent.futures as cf
+import numpy as np
+import unittest
+
+def evolve_and_return(args):
+    """
+    This function runs our simulation.
+    The input arguments come in a tuple,
+    which is required by many of Python's
+    functions for execution in separate processes.
+
+    For this function, the arguments are the population
+    size and a random number seed.
+    """
+    N,seed=args
+    #Construct as single-deme object
+    #with N diploids
+    pop = fp11.Spop(N)
+    #Initialize a random number generator
+    rng=fp11.GSLrng(seed)
+    sregions=[fp11.ExpS(0,1,1,-0.1,1.0)]
+    #Evolve the pop for 10N generations
+    #and several default parameters will be
+    #used.
+    fp11.wright_fisher.evolve(rng,pop,sregions=sregions)
+    #The population is picklable, and so
+    #we can return it from another process
+    return pop 
+
+class testSimpleParallel(unittest.TestCase):
+    """
+    This would be your __name__=="__main__"
+    in a script
+    """
+    def test_simple_parallel(self):
+        #init global rng seed
+        np.random.seed(101)
+        #Generate a list of arguments for our processes.
+        #We generation 10 random number seeds
+        args=[(1000,seed) for seed in np.random.randint(0,42000000,10)]
+        #Use a process pool with a max of 10 workers
+        with cf.ProcessPoolExecutor(10) as pool:
+            #Run our simulations and get the 
+            #result back, which will be
+            #the population
+            for res in pool.map(evolve_and_return,args):
+                self.assertEqual(str(type(res)),
+                        "<class 'fwdpy11.fwdpy11_types.Spop'>")
+                
+if __name__ == "__main__":
+    unittest.main()
