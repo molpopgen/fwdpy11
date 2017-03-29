6e96bfaf1895571abc6edf13ed0dddf632e43b70
molpopgen <krthornt@uci.edu>
2017-03-29 07:57:22 -0700

    document example evolve function

--- examples/evolve_with_sampler.py
+++ examples/evolve_with_sampler.py
@@ -4,6 +4,12 @@ import fwdpy11.wright_fisher
 import numpy as np
 
 def evolve_and_return_with_sampler(args):
+    """
+    Evolve function takes an initial
+    population size, replicate ID number,
+    RNG seed, and the type name of a recorder as
+    arguments.
+    """
     N,repid,seed,recorderType=args
     pop = fp11.Spop(N)
     rng=fp11.GSLrng(seed)
@@ -12,6 +18,8 @@ def evolve_and_return_with_sampler(args):
     recregions=nregions
     fitness=fp11w.SpopAdditive(2.0)
     nlist=np.array([N]*N,dtype=np.uint32)
+    #use the type name passed in 
+    #to create an instance:
     recorder=recorderType()
     fp11.wright_fisher.evolve_regions_sampler_fitness(rng,pop,
             nlist,0.001,0.005,0.001,nregions,sregions,recregions,fitness,recorder)
