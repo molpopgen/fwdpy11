8247247617bb8c4947ae8dd79d41a68660a24aca
molpopgen <krthornt@uci.edu>
2017-04-03 16:18:56 -0700

    added single-region trait models

--- fwdpy11/src/fwdpy11_fitness.cc
+++ fwdpy11/src/fwdpy11_fitness.cc
@@ -24,20 +24,72 @@
 
 namespace py = pybind11;
 
+struct additive_diploid_trait_fxn
+{
+    inline double
+    operator()(const fwdpy11::diploid_t &dip, const fwdpy11::gcont_t &gametes,
+               const fwdpy11::mcont_t &mutations, const double scaling) const
+    {
+        return KTfwd::site_dependent_fitness()(
+            dip, gametes, mutations,
+            [scaling](double &fitness,
+                      const fwdpy11::mcont_t::value_type &mut) noexcept {
+                fitness += (scaling * mut.s);
+            },
+            [](double &fitness,
+               const fwdpy11::mcont_t::value_type &mut) noexcept {
+                fitness += (mut.h * mut.s);
+            },
+            0.);
+    }
+};
+
+struct multiplicative_diploid_trait_fxn
+{
+    inline double
+    operator()(const fwdpy11::diploid_t &dip, const fwdpy11::gcont_t &gametes,
+               const fwdpy11::mcont_t &mutations, const double scaling) const
+    {
+        return KTfwd::site_dependent_fitness()(
+                   dip, gametes, mutations,
+                   [scaling](
+                       double &fitness,
+                       const fwdpy11::mcont_t::value_type &mut) noexcept {
+                       fitness *= (1. + scaling * mut.s);
+                   },
+                   [](double &fitness,
+                      const fwdpy11::mcont_t::value_type &mut) noexcept {
+                       fitness *= (1. + mut.h * mut.s);
+                   },
+                   1.)
+               - 1.0;
+    }
+};
+
+using singlepop_multiplicative_trait_wrapper = fwdpy11::
+    fwdpp_singlepop_fitness_wrapper<multiplicative_diploid_trait_fxn>;
+using singlepop_additive_trait_wrapper
+    = fwdpy11::fwdpp_singlepop_fitness_wrapper<additive_diploid_trait_fxn>;
+
 PYBIND11_PLUGIN(fitness)
 {
     py::module m("fitness", "Fitness models.");
 
     py::class_<fwdpy11::singlepop_fitness>(m, "SpopFitness",
-            R"delim(
+                                           R"delim(
             A fitness function or trait value function
             for a single-deme, single-region simulation (
             :class:`fwdpy11.fwdpy11_types.Spop`).
             
-            Current objects derived from this type are:
+            Current fitness ovjects derived from this type are:
             
             * :class:`fwdpy11.fitness.SpopAdditive`
             * :class:`fwdpy11.fitness.SpopMult`
+            
+            Current trait value objects derived from this type are:
+
+            * :class:`fwdpy11.fitness.SpopAdditiveTrait`
+            * :class:`fwdpy11.fitness.SpopMultTrait`
             )delim");
 
     py::class_<fwdpy11::singlepop_mult_wrapper, fwdpy11::singlepop_fitness>(
@@ -55,8 +107,7 @@ PYBIND11_PLUGIN(fitness)
         .def(py::init<double>(), py::arg("scaling"));
 
     py::class_<fwdpy11::singlepop_additive_wrapper,
-               fwdpy11::singlepop_fitness>(
-        m, "SpopAdditive", R"delim(
+               fwdpy11::singlepop_fitness>(m, "SpopAdditive", R"delim(
         Additive fitness for single-deme simulations.
         Fitness is max(0,1 + :math:`\sum_{i} x_i`), 
         where :math:`x_i = 0, sh,\ \mathrm{or\ }scaling \times s`
@@ -67,6 +118,24 @@ PYBIND11_PLUGIN(fitness)
             import fwdpy11.fitness as fp11w
             w = fp11w.SpopAdditive(2.0)
         )delim")
+        .def(py::init<double>(), py::arg("scaling"));
+
+    py::class_<singlepop_additive_trait_wrapper, fwdpy11::singlepop_fitness>(
+        m, "SpopAdditiveTrait",
+        R"delim(
+                Additive trait value, centered on zero.
+
+                Trait value is :math:`\sum_{i} x_i`), 
+                where :math:`x_i = 0, sh,\ \mathrm{or\ }scaling \times s`
+                for AA, Aa, and aa, respectively.
+                )delim")
+        .def(py::init<double>(), py::arg("scaling"));
+
+    py::class_<singlepop_multiplicative_trait_wrapper,
+               fwdpy11::singlepop_fitness>(m, "SpopMultTrait",
+                                           R"delim(
+                Multiplicative trait value, centered on zero.
+                )delim")
         .def(py::init<double>(), py::arg("scaling"));
 
     return m.ptr();
