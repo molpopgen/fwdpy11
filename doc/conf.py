01840f524b7ab6d6082e35455d31598ca20adb11
molpopgen <krthornt@uci.edu>
2017-04-07 10:46:06 -0700

    enable manual building locally (e.g., not on RTD)

--- doc/conf.py
+++ doc/conf.py
@@ -111,12 +111,13 @@ todo_include_todos = True
 #
 html_theme = 'default'
 
-html_theme_options = {
-        'github_user':'molpopgen',
-        'github_repo':'fwdpy11',
-#        'github_button':True,
-#        'github_banner':True,
-        }
+if (os.environ.get('READTHEDOCS')=="True") is True:
+    html_theme_options = {
+            'github_user':'molpopgen',
+            'github_repo':'fwdpy11',
+    #        'github_button':True,
+    #        'github_banner':True,
+            }
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
