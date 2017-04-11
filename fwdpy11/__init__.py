0e7a68b6e07811a7e42d14acb12ffdd17343a4a9
molpopgen <krthornt@uci.edu>
2017-04-10 17:44:45 -0700

    get 0.1-post1 ready

--- fwdpy11/__init__.py
+++ fwdpy11/__init__.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with fwdpy11.  If not, see <http://www.gnu.org/licenses/>.
 #
-__version__='0.1'
+__version__='0.1-post1'
 from .fwdpp_types import *
 from .fwdpy11_types import *
 from .regions import *
