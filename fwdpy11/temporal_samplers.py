a0dbd1e647e178b30805336bc2ebe4520b3d4e6d
molpopgen <krthornt@uci.edu>
2017-06-20 09:06:07 -0700

    file now passes flake8 lint checks

--- fwdpy11/temporal_samplers.py
+++ fwdpy11/temporal_samplers.py
@@ -16,9 +16,14 @@
 # You should have received a copy of the GNU General Public License
 # along with fwdpy11.  If not, see <http://www.gnu.org/licenses/>.
 #
+
+
 class RecordNothing:
     """
     A temporal sampler that does nothing.
     """
-    def __call__(self,pop):
+    def __call__(self, pop):
+        """
+        Returns the C++ callback for this DFE
+        """
         return
