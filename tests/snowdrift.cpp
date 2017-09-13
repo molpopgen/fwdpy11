63f07855be2693db835b796615ebbce245f6384b
molpopgen <krthornt@uci.edu>
2017-09-13 14:49:57 -0700

    fix handling of include dirs for cpp code in unit tests

--- tests/snowdrift.cpp
+++ tests/snowdrift.cpp
@@ -21,7 +21,7 @@ setup_pybind11(cfg)
 #import fwdpy11 so we can find its C++ headers
 import fwdpy11 as fp11 
 #add fwdpy11 header locations to the include path
-cfg['include_dirs'] = [ fp11.get_includes(), fp11.get_fwdpp_includes() ] 
+cfg['include_dirs'].extend([ fp11.get_includes(), fp11.get_fwdpp_includes()])
 #On OS X using clang, there is more work to do.  Using gcc on OS X
 #gets rid of these requirements. The specifics sadly depend on how
 #you initially built fwdpy11, and what is below assumes you used
