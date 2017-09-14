6527c0bcaf5ef29e360c731da97d00bb71e07e9a
molpopgen <krthornt@uci.edu>
2017-09-14 15:30:47 -0700

    fix deprecation warning

--- tests/quick_pops.py
+++ tests/quick_pops.py
@@ -66,7 +66,7 @@ def quick_slocus_qtrait_pop_params(N=1000, simlen=100):
          'rates': (0.0, 2e-3, 1e-3),
          'demography': np.array([N] * simlen, dtype=np.uint32),
          'gvalue': SlocusAdditiveTrait(2.0),
-         'trait_to_fitness': GSS(1, 0)
+         'trait2w': GSS(1, 0)
          }
     pop = SlocusPop(N)
     params = SlocusParamsQ(**p)
