c7ebd28ade2eeaf2b0f039dfa8052724563957d1
molpopgen <krthornt@uci.edu>
2017-09-26 16:00:28 -0700

    add an echo

--- travis_scripts/installation_script.sh
+++ travis_scripts/installation_script.sh
@@ -2,6 +2,7 @@
 
 if [ "$USECONDA" == "1" ];
 then
+    echo "HERE"
     if [ "$TRAVIS_OS_NAME" == "linux" ]; 
     then 
         wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
