b20339caec175a7cd871545d1b7fb3ca2d8c990c
molpopgen <krthornt@uci.edu>
2018-03-26 16:34:25 -0700

    s/KTfwd/fwdpp in header

--- fwdpy11/src/fwdpy11_util_add_mutation.hpp
+++ fwdpy11/src/fwdpy11_util_add_mutation.hpp
@@ -4,12 +4,12 @@
 #include <fwdpy11/types.hpp>
 
 std::size_t add_mutation(const fwdpy11::GSLrng_t& rng, fwdpy11::singlepop_t& pop,
-                  const KTfwd::uint_t ncopies,
+                  const fwdpp::uint_t ncopies,
                   const std::tuple<double, double, double>& pos_s_h,
                   const std::uint16_t label);
 std::size_t
 add_mutation(const fwdpy11::GSLrng_t& rng, fwdpy11::multilocus_t& pop,
-             const std::size_t locus, const KTfwd::uint_t ncopies,
+             const std::size_t locus, const fwdpp::uint_t ncopies,
              const std::tuple<double, double, double>& pos_s_h,
              const std::uint16_t label);
              
