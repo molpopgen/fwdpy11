3322a27678e31c4ee81201727efa0c502a69b7b9
molpopgen <krthornt@uci.edu>
2018-04-03 14:40:16 -0700

    update makeMutationRegions to accept new required arguments and pass on data in proper format

--- fwdpy11/internal.py
+++ fwdpy11/internal.py
@@ -18,10 +18,12 @@
 #
 
 
-def makeMutationRegions(neutral, selected):
+def makeMutationRegions(rng, pop, neutral, selected):
     """
     Convert user input into :class:`~fwdpy11.fwdpp_extensions.MutationRegions`
 
+    :param rng: A :class:`fwdpy11.GSLrng`
+    :param pop: A :class:`fwdp11.Population`
     :param neutral: A list of :class:`fwdpy11.regions.Region` objects.
     :param selected: A list of :class:`fwdpy11.regions.Sregion` objects.
 
@@ -48,17 +50,11 @@ def makeMutationRegions(neutral, selected):
             ...
         TypeError: 'NoneType' object is not iterable
     """
-    nbeg = [i.b for i in neutral]
-    nend = [i.e for i in neutral]
-    nweights = [i.w for i in neutral]
-    nlabels = [i.l for i in neutral]
-    sbeg = [i.b for i in selected]
-    send = [i.e for i in selected]
-    sweights = [i.w for i in selected]
-    sh = [i.callback() for i in selected]
-    slabels = [i.l for i in selected]
+    ntuples = [(i.b, i.e, i.w, i.l) for i in neutral]
+    stuples = [(i.b, i.e, i.w, i.l) for i in selected]
+    callbacks = [i.callback() for i in selected]
     from .fwdpp_extensions import MutationRegions
-    return MutationRegions(nbeg, nend, nweights, sbeg, send, sweights, sh, nlabels, slabels)
+    return MutationRegions(rng, pop, ntuples, stuples, callbacks)
 
 
 def makeRecombinationRegions(rng, recrate, regions):
