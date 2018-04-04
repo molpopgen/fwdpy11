1ed9fbeb729e8b25af72f20239e4e29feac49c72
molpopgen <krthornt@uci.edu>
2018-04-04 13:56:04 -0700

    new pop headers added to get SlocusPop in place

---
+++ fwdpy11/headers/fwdpy11/types/PyPopulation.hpp
@@ -0,0 +1,53 @@
+#ifndef FWDPY11_PYPOPULATION_HPP__
+#define FWDPY11_PYPOPULATION_HPP__
+
+#include <pybind11/pybind11.h>
+#include <fwdpp/forward_types.hpp>
+#include <fwdpp/sugar/poptypes/popbase.hpp>
+
+namespace fwdpy11
+{
+    template <typename mutation_type, typename mcont, typename gcont,
+              typename mvector, typename ftvector, typename lookup_table_type>
+    class PyPopulation
+        : public fwdpp::sugar::popbase<mutation_type, mcont, gcont, mvector,
+                                       ftvector, lookup_table_type>
+    // Abstract base class (ABC) for population types
+    {
+      private:
+        virtual void process_individual_input() = 0;
+        using fwdpp_base
+            = fwdpp::sugar::popbase<mutation_type, mcont, gcont, mvector,
+                                    ftvector, lookup_table_type>;
+
+      public:
+        fwdpp::uint_t N;
+        fwdpp::uint_t generation;
+        pybind11::object popdata;
+        pybind11::object popdata_user;
+
+        virtual ~PyPopulation() = default;
+
+        PyPopulation(PyPopulation &&) = default;
+        PyPopulation(const PyPopulation &) = default;
+
+        PyPopulation(fwdpp::uint_t N_)
+            : fwdpp_base{ N_ }, N{ N_ }, generation{ 0 },
+              popdata{ pybind11::none() }, popdata_user{ pybind11::none() }
+        {
+        }
+
+        template <typename gametes_input, typename mutations_input>
+        explicit PyPopulation(
+            const fwdpp::uint_t N_, gametes_input &&g, mutations_input &&m,
+            typename gamete_t::mutation_container::size_type reserve_size)
+            : fwdpp_base{ std::forward<gametes_input>(g),
+                          std::forward<mutations_input>(m), reserve_size },
+              N{ N_ }, generation{ 0 }, popdata{ pybind11::none() },
+              popdata_user{ pybind11::none() }
+        {
+        }
+    };
+}
+
+#endif
---
+++ fwdpy11/headers/fwdpy11/types/SlocusPop.hpp
@@ -0,0 +1,92 @@
+#ifndef FWDPY11_SLOCUSPOP_HPP__
+#define FWDPY11_SLOCUSPOP_HPP__
+
+#include "Population.hpp"
+#include "Diploid.hpp"
+#include <fwdpp/sugar/poptypes/tags.hpp>
+
+namespace fwdpy11
+{
+    class SlocusPop : public Population
+    {
+      private:
+        void
+        process_individual_input()
+        {
+            std::vector<fwdpp::uint_t> gcounts(this->gametes.size(), 0);
+            for (auto &&dip : diploids)
+                {
+                    this->validate_individual_keys(dip.first);
+                    this->validate_individual_keys(dip.second);
+                    gcounts[dip.first]++;
+                    gcounts[dip.second]++;
+                }
+            this->validate_gamete_counts(gcounts);
+        }
+
+      public:
+        using dipvector_t = std::vector<Diploid>;
+        using diploid_t = dipvector_t::value_type;
+        using popbase_t = Population;
+        using popmodel_t = fwdpp::sugar::SINGLELOC_TAG;
+        using fitness_t
+            = fwdpp::traits::fitness_fxn_t<dipvector_t,
+                                           typename popbase_t::gcont_t,
+                                           typename popbase_t::mcont_t>;
+
+        dipvector_t diploids;
+
+        SlocusPop(SlocusPop &&) = default;
+        SlocusPop(const SlocusPop &) = default;
+        SlocusPop &operator=(SlocusPop &&) = default;
+        SlocusPop &operator=(const SlocusPop &) = default;
+
+        // Constructors for Python
+        SlocusPop(const fwdpp::uint_t N) : Population{ N } {}
+
+        template <typename diploids_input, typename gametes_input,
+                  typename mutations_input>
+        explicit SlocusPop(diploids_input &&d, gametes_input &&g,
+                           mutations_input &&m)
+            : Population(static_cast<fwdpp::uint_t>(d.size()),
+                         std::forward<gametes_input>(g),
+                         std::forward<mutations_input>(m), 100),
+              diploids(std::forward<diploids_input>(d))
+        //! Constructor for pre-determined population status
+        {
+            this->process_individual_input();
+        }
+
+        bool
+        operator==(const SlocusPop &rhs) const
+        {
+            return this->diploids == rhs.diploids && popbase_t::is_equal(rhs);
+        };
+
+        void
+        clear()
+        {
+            diploids.clear();
+            popbase_t::clear_containers();
+        }
+
+        static SlocusPop
+        create(dipvector_t &diploids, gcont_t &gametes, mcont_t &mutations)
+        {
+            return create_wrapper<SlocusPop>(
+                std::move(diploids), std::move(gametes), std::move(mutations));
+        }
+
+        static SlocusPop
+        create_with_fixations(dipvector_t &diploids, gcont_t &gametes,
+                              mcont_t &mutations, mcont_t &fixations,
+                              std::vector<fwdpp::uint_t> &fixation_times,
+                              const fwdpp::uint_t generation)
+        {
+            return create_wrapper<SlocusPop>(
+                std::move(diploids), std::move(gametes), std::move(mutations),
+                fixations, fixation_times, generation);
+        }
+    };
+}
+#endif
