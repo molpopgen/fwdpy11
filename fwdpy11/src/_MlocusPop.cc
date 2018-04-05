bc89f3bab0ab7ef4b30fb008afb0fb45600da877
molpopgen <krthornt@uci.edu>
2018-04-05 06:36:23 -0700

    Pickling for MlocusPop

--- fwdpy11/src/_MlocusPop.cc
+++ fwdpy11/src/_MlocusPop.cc
@@ -22,6 +22,8 @@
 #include <pybind11/stl_bind.h>
 #include <fwdpy11/types/MlocusPop.hpp>
 #include <fwdpy11/types/create_pops.hpp>
+#include <fwdpy11/serialization.hpp>
+#include <fwdpy11/serialization/Diploid.hpp>
 
 namespace py = pybind11;
 
@@ -42,8 +44,8 @@ PYBIND11_MODULE(_MlocusPop, m)
 
     py::class_<fwdpy11::MlocusPop, fwdpy11::Population>(
         m, "_MlocusPop", "Representation of a multi-locus population")
-        .def(py::init<fwdpp::uint_t, fwdpp::uint_t>(),
-             py::arg("N"), py::arg("nloci"))
+        .def(py::init<fwdpp::uint_t, fwdpp::uint_t>(), py::arg("N"),
+             py::arg("nloci"))
         .def(py::init<fwdpp::uint_t, fwdpp::uint_t,
                       const std::vector<std::pair<double, double>>&>(),
              py::arg("N"), py::arg("nloci"), py::arg("locus_boundaries"))
@@ -72,19 +74,62 @@ PYBIND11_MODULE(_MlocusPop, m)
                       &fwdpy11::MlocusPop::locus_boundaries)
         .def_readonly("diploids", &fwdpy11::MlocusPop::diploids,
                       DIPLOIDS_DOCSTRING)
-        .def_static("create", [](fwdpy11::MlocusPop::dipvector_t& diploids,
-                                 fwdpy11::MlocusPop::gcont_t& gametes,
-                                 fwdpy11::MlocusPop::mcont_t& mutations,
-                                 py::tuple args) -> fwdpy11::MlocusPop {
-            if (args.size() == 0)
-                {
-                    return fwdpy11::create_wrapper<fwdpy11::MlocusPop>()(
-                        diploids, gametes, mutations);
-                }
-            auto fixations = args[0].cast<fwdpy11::MlocusPop::mcont_t>();
-            auto ftimes = args[1].cast<std::vector<fwdpp::uint_t>>();
-            auto g = args[2].cast<fwdpp::uint_t>();
-            return fwdpy11::create_wrapper<fwdpy11::MlocusPop>()(
-                diploids, gametes, mutations, fixations, ftimes, g);
-        });
+        .def_static(
+            "create",
+            [](fwdpy11::MlocusPop::dipvector_t& diploids,
+               fwdpy11::MlocusPop::gcont_t& gametes,
+               fwdpy11::MlocusPop::mcont_t& mutations,
+               py::tuple args) -> fwdpy11::MlocusPop {
+                if (args.size() == 0)
+                    {
+                        return fwdpy11::create_wrapper<fwdpy11::MlocusPop>()(
+                            diploids, gametes, mutations);
+                    }
+                auto fixations = args[0].cast<fwdpy11::MlocusPop::mcont_t>();
+                auto ftimes = args[1].cast<std::vector<fwdpp::uint_t>>();
+                auto g = args[2].cast<fwdpp::uint_t>();
+                return fwdpy11::create_wrapper<fwdpy11::MlocusPop>()(
+                    diploids, gametes, mutations, fixations, ftimes, g);
+            })
+        .def(py::pickle(
+            [](const fwdpy11::MlocusPop& pop) -> py::object {
+                auto pb = py::bytes(
+                    fwdpy11::serialization::serialize_details(&pop));
+                py::list pdata;
+                for (auto& d : pop.diploids)
+                    {
+                        pdata.append(d[0].parental_data);
+                    }
+                return py::make_tuple(std::move(pb), std::move(pdata),
+                                      pop.popdata, pop.popdata_user);
+            },
+            [](py::object pickled) -> fwdpy11::MlocusPop {
+                try
+                    {
+                        auto s = pickled.cast<py::bytes>();
+                        return fwdpy11::serialization::deserialize_details<
+                            fwdpy11::MlocusPop>()(s, 1, 1);
+                    }
+                catch (std::runtime_error& eas)
+                    {
+                        PyErr_Clear();
+                    }
+                auto t = pickled.cast<py::tuple>();
+                if (t.size() != 4)
+                    {
+                        throw std::runtime_error(
+                            "expected tuple with 4 elements");
+                    }
+                auto s = t[0].cast<py::bytes>();
+                auto l = t[1].cast<py::list>();
+                auto rv = fwdpy11::serialization::deserialize_details<
+                    fwdpy11::MlocusPop>()(s, 1, 1);
+                for (std::size_t i = 0; i < rv.diploids.size(); ++i)
+                    {
+                        rv.diploids[i][0].parental_data = l[i];
+                    }
+                rv.popdata = t[2];
+                rv.popdata_user = t[3];
+                return rv;
+            }));
 }
