9bec24081e0816e8ff21caaa355d423a0cc42c14
molpopgen <krthornt@uci.edu>
2018-04-06 09:23:28 -0700

    change Population to depend on Mutation instead of fwdpp::popgenmut

--- fwdpy11/headers/fwdpy11/types/Population.hpp
+++ fwdpy11/headers/fwdpy11/types/Population.hpp
@@ -2,17 +2,17 @@
 #define FWDPY11_POPULATION_HPP__
 
 #include "PyPopulation.hpp"
+#include "Mutation.hpp"
 #include <unordered_set>
 #include <fwdpp/fwd_functional.hpp>
-#include <fwdpp/sugar/popgenmut.hpp>
 
 namespace fwdpy11
 {
 	// Population abstract base class based on
 	// fwdpy11's Mutation type.
     using Population
-        = PyPopulation<fwdpp::popgenmut, std::vector<fwdpp::popgenmut>,
-                       std::vector<fwdpp::gamete>, std::vector<fwdpp::popgenmut>,
+        = PyPopulation<Mutation, std::vector<Mutation>,
+                       std::vector<fwdpp::gamete>, std::vector<Mutation>,
                        std::vector<fwdpp::uint_t>,
                        std::unordered_set<double, std::hash<double>,
                                           fwdpp::equal_eps>>;
