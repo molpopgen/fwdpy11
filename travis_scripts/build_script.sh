2eeb7bce2df65815de4d220e9c5dacf7aa97c537
molpopgen <krthornt@uci.edu>
2018-04-09 15:06:53 -0700

    do not attempt doctest on Travis.  It just takes too long

--- travis_scripts/build_script.sh
+++ travis_scripts/build_script.sh
@@ -14,7 +14,7 @@ then
     export LDFLAGS="-L$HOME/miniconda/lib $LDFLAGS"
     if [ "$TRAVIS_OS_NAME" == "linux" ]; then CC=$CC CXX=$CXX python setup.py build_ext -i; fi
     if [ "$TRAVIS_OS_NAME" == "linux" ];then CC=$CC CXX=$CXX python -m unittest discover -v tests; fi
-    if [ "$TRAVIS_OS_NAME" == "linux" ];then cd doc && make doctest; fi
+    # if [ "$TRAVIS_OS_NAME" == "linux" ];then cd doc && make doctest; fi
     if [ "$TRAVIS_OS_NAME" == "osx" -a "$OSXGCC" == "0" ]; then python setup.py build_ext -i; fi
     if [ "$TRAVIS_OS_NAME" == "osx" -a "$OSXGCC" == "1" ]; then CC=gcc CXX=g++ python setup.py build_ext -i --gcc; fi
     if [ "$TRAVIS_OS_NAME" == "osx" -a "$OSXGCC" == "0" ]; then LDFLAGS='-stdlib=libc++ -mmacosx-version-min=10.7' CPPFLAGS="-stdlib=libc++ -mmacosx-version-min=10.7" python -m unittest discover -v tests; fi
