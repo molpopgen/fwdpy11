31e66e7529acd1b3078c9ca3245aa705c103b594
molpopgen <krthornt@uci.edu>
2018-04-09 14:58:15 -0700

    use older miniconda and attempt a minimal installation on Travis

--- travis_scripts/installation_script.sh
+++ travis_scripts/installation_script.sh
@@ -5,17 +5,17 @@ then
     echo "HERE"
     if [ "$TRAVIS_OS_NAME" == "linux" ]; 
     then 
-        wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
+        wget https://repo.continuum.io/miniconda/Miniconda3-4.3.21-Linux-x86_64.sh -O miniconda.sh;
     fi
     if [ "$TRAVIS_OS_NAME" == "osx" ];
     then 
-        wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh;
+        wget https://repo.continuum.io/miniconda/Miniconda3-4.3.21-MacOSX-x86_64.sh -O miniconda.sh;
     fi
     bash miniconda.sh -b -p $HOME/miniconda
     export PATH="$HOME/miniconda/bin:$PATH"
     hash -r
     conda config --set always_yes yes --set changeps1 no
-    conda update -q conda
+    #conda update -q conda
     # Useful for debugging any issues with conda
     conda info -a
     if [ "$TRAVIS_OS_NAME" == "linux" ];
@@ -27,7 +27,7 @@ then
     fi
     conda install cython numpy python==3.6 gsl
     conda install -c conda-forge pybind11==2.2.2
-    conda install -c conda-forge sphinx nbsphinx ipython matplotlib msprime
+    # conda install -c conda-forge sphinx nbsphinx ipython matplotlib msprime
     pip install cppimport
     echo `which python`
     echo `which python3`
