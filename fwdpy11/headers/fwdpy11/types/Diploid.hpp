d58e32900c9ac1ee8f2748bd70abe7a8b461bd65
molpopgen <krthornt@uci.edu>
2018-06-08 15:08:57 -0700

    Strip diploid type to minimum and add metadata.

--- fwdpy11/headers/fwdpy11/types/Diploid.hpp
+++ fwdpy11/headers/fwdpy11/types/Diploid.hpp
@@ -8,73 +8,20 @@
 
 namespace fwdpy11
 {
-    struct Diploid
+    using Diploid = std::pair<std::size_t, std::size_t>;
+
+    struct diploid_metadata
     /*!
-      \brief Custom diploid type.
+      \brief Bare struct for metadata.  Used as Numpy dtype.
     */
     {
-        using first_type = std::size_t;
-        using second_type = std::size_t;
-        //! First gamete.  A gamete is vector<size_t> where the elements are
-        //! indexes to a population's gamete container
-        first_type first;
-        //! Second gamete. A gamete is vector<size_t> where the elements are
-        //! indexes to a population's gamete container
-        second_type second;
-        //! 64 bits of data to do stuff with.  Initialized to zero upon
-        //! construction
-        std::size_t label;
+        double g; // Genetic value
+        double e; // Random component of trait value
+        double w; // Fitness
+        std::size_t label;  // Index of individual in pop container
+        std::size_t parents[2]; // Indexes of parents
         std::uint32_t deme;
         std::int32_t sex;
-        //! Genetic component of trait value.  This is not necessarily written
-        //! to by a simulation.
-        double g;
-        //! Random component of trait value.  This is not necessarily written
-        //! to by a simulation.
-        double e;
-        //! Fitness.  This is not necessarily written to by a simulation.
-        double w;
-        //! IDs of parents.  NB: this will be changed in future releases
-        std::tuple<std::size_t, std::size_t> parental_data;
-        //! Constructor
-        Diploid() noexcept
-            : first(first_type()), second(second_type()), label(0), deme(0),
-              sex(-1), g(0.), e(0.), w(1.), parental_data{}
-        {
-        }
-        //! Construct from two indexes to gametes
-        Diploid(first_type g1, first_type g2) noexcept
-            : first(g1), second(g2), label(0), deme(0), sex(-1), g(0.), e(0.),
-              w(1.), parental_data{}
-        {
-        }
-
-        Diploid(first_type g1, first_type g2, std::size_t label_, double deme_,
-                double sex_, double g_, double e_, double w_)
-            : first(g1), second(g2), label(label_), deme(deme_), sex(sex_),
-              g(g_), e(e_), w(w_), parental_data{}
-        {
-        }
-
-        static inline Diploid
-        create(first_type g1, first_type g2, std::size_t label_, double deme_,
-               double sex_, double g_, double e_, double w_)
-        {
-            return Diploid(g1, g2, label_, deme_, sex_, g_, e_, w_);
-        }
-
-        inline bool
-        operator==(const Diploid& dip) const noexcept
-        //! Required for py::bind_vector
-        {
-            auto cpp_data_comparison
-                = this->first == dip.first && this->second == dip.second
-                  && this->w == dip.w && this->g == dip.g && this->e == dip.e
-                  && this->label == dip.label && this->deme == dip.deme
-                  && this->sex == dip.sex
-                  && this->parental_data == dip.parental_data;
-            return cpp_data_comparison;
-        }
     };
 
     //! Typedef for container of diploids
