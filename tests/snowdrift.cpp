cc58be7a37d17f448a3b737e6b3712bf7c379f2e
molpopgen <krthornt@uci.edu>
2018-06-20 12:17:23 -0700

    Clean up snowdrift C++ code

--- tests/snowdrift.cpp
+++ tests/snowdrift.cpp
@@ -41,61 +41,6 @@ cfg['include_dirs'].extend([ fp11.get_includes(), fp11.get_fwdpp_includes()])
 
     namespace py = pybind11;
 
-struct snowdrift_diploid
-/* This is a function object implementing the snowdrift
- * fitness calculation
- */
-{
-    using result_type = double;
-    inline result_type
-    operator()(const fwdpy11::DiploidGenotype &dip,
-               const fwdpy11::Population::gcont_t &gametes,
-               const fwdpy11::Population::mcont_t &mutations,
-               const fwdpy11::DiploidMetadata&metadata,
-               const std::vector<double> &phenotypes, const double b1,
-               const double b2, const double c1, const double c2) const
-    /* The first 3 arguments will be passed in from fwdpp.
-     * The phenotypes are the stateful part, and will get
-     * passed in by reference.  The remaining params
-     * define the payoff function and are also supplied externally.
-     */
-    {
-        auto N = phenotypes.size();
-        // A diploid tracks its index via
-        // fwdpy11::Diploid::label, which
-        // is std::size_t.
-        auto i = metadata.label;
-        double zself = phenotypes[i];
-        // The code here would not be found
-        // in production code.  Here, we are testing
-        // that all the data have been updated correctly
-        // in fwdpy11's machinery for evolving populations.
-        // This test is only here as this is part of unit
-        // testing suite.
-        auto g = fwdpp::additive_diploid(2.0)(dip, gametes, mutations);
-        if (zself != g)
-            {
-                throw std::runtime_error("snowdrift not working: "
-                                         + std::to_string(zself) + ' '
-                                         + std::to_string(g));
-            }
-        // End unit-testing only code
-        result_type fitness = 0;
-        for (std::size_t j = 0; j < N; ++j)
-            {
-                if (i != j)
-                    {
-                        double zpair = zself + phenotypes[j];
-                        // Payoff function from Fig 1
-                        double a = b1 * zpair + b2 * zpair * zpair - c1 * zself
-                                   - c2 * zself * zself;
-                        fitness += 1 + std::max(a, 0.0);
-                    }
-            }
-        return fitness / double(N - 1);
-    }
-};
-
 struct snowdrift : public fwdpy11::SlocusPopGeneticValue
 /* This is our stateful fitness object.
  * It records the model parameters and holds a
@@ -150,7 +95,7 @@ struct snowdrift : public fwdpy11::SlocusPopGeneticValue
 
     inline double
     noise(const fwdpy11::GSLrng_t & /*rng*/,
-          const fwdpy11::DiploidMetadata& /*offspring_metadata*/,
+          const fwdpy11::DiploidMetadata & /*offspring_metadata*/,
           const std::size_t /*parent1*/, const std::size_t /*parent2*/,
           const fwdpy11::SlocusPop & /*pop*/) const
     {
@@ -181,34 +126,13 @@ PYBIND11_MODULE(snowdrift, m)
 {
     m.doc() = "Example of custom stateful fitness model.";
 
-    //pybind11::object FWDPY11_SINGLE_LOCUS_FITNESS_BASE_IMPORT__               \
-    //    = (pybind11::object)pybind11::module::import("fwdpy11.fitness")       \
     //          .attr("SlocusFitness");
-    try
-        {
-            pybind11::object imported_base
-                = pybind11::module::import("fwdpy11.genetic_values")
-                      .attr("fwdpy11.genetic_values.SlocusPopGeneticValue");
-        }
-    catch (...)
-        {
-        }
+    pybind11::object imported_snowdrift_base_class_type
+        = pybind11::module::import("fwdpy11.genetic_values")
+              .attr("SlocusPopGeneticValue");
     // Create a Python class based on our new type
     py::class_<snowdrift, fwdpy11::SlocusPopGeneticValue>(m, "SlocusSnowdrift")
         .def(py::init<double, double, double, double>(), py::arg("b1"),
              py::arg("b2"), py::arg("c1"), py::arg("c2"))
         .def_readwrite("phenotypes", &snowdrift::phenotypes);
-    // It is useful to make these type compatible with Python's
-    // pickling protocol:
-    //.def(py::pickle(
-    //    [](const snowdrift &s) {
-    //        return py::make_tuple(s.b1, s.b2, s.c1, s.c2, s.phenotypes);
-    //    },
-    //    [](py::tuple t) {
-    //        auto rv = std::make_shared<snowdrift>(
-    //            t[0].cast<double>(), t[1].cast<double>(),
-    //            t[2].cast<double>(), t[3].cast<double>());
-    //        rv->phenotypes = t[4].cast<std::vector<double>>();
-    //        return rv;
-    //    }));
 }
