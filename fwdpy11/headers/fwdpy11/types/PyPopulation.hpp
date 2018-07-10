165f1b243be230ca28f27a8cd4bc469952525687
molpopgen <krthornt@uci.edu>
2018-07-10 16:05:37 -0700

    Add API requirements for generating fwdpp::data_matrix objects from Populations.

--- fwdpy11/headers/fwdpy11/types/PyPopulation.hpp
+++ fwdpy11/headers/fwdpy11/types/PyPopulation.hpp
@@ -3,8 +3,11 @@
 
 #include <tuple>
 #include <algorithm>
+#include <gsl/gsl_randist.h>
 #include <fwdpp/forward_types.hpp>
 #include <fwdpp/sugar/poptypes/popbase.hpp>
+#include <fwdpp/sugar/matrix.hpp>
+#include "../rng.hpp"
 #include "Diploid.hpp"
 
 namespace fwdpy11
@@ -87,6 +90,76 @@ namespace fwdpy11
                 return -1;
             return static_cast<std::int64_t>(
                 std::distance(this->fixations.begin(), itr));
+        }
+
+        virtual fwdpp::data_matrix
+        sample_individuals(const std::vector<std::size_t> &,
+                           const bool) const = 0;
+        virtual fwdpp::data_matrix
+        sample_random_individuals(const GSLrng_t &, const std::uint32_t,
+                                  const bool) const = 0;
+
+        // The next two functions can be called from derived
+        // classes to help implementing the above two functions.
+        template <typename poptype>
+        fwdpp::data_matrix
+        sample_individuals_details(const poptype &pop,
+                                   const std::vector<std::size_t> &individuals,
+                                   const bool haplotype) const
+        {
+            if (std::any_of(individuals.begin(), individuals.end(),
+                            [&pop](const std::size_t i) {
+                                return i >= pop.diploids.size();
+                            }))
+                {
+                    throw std::out_of_range("individual index out of range");
+                }
+            auto keys = fwdpp::mutation_keys(pop, individuals, true, true);
+            //sort keys on position
+            std::sort(keys.first.begin(), keys.first.end(),
+                      [&pop](const decltype(keys.first[0]) &a,
+                             const decltype(keys.first[0]) &b) {
+                          return pop.mutations[a.first].pos
+                                 < pop.mutations[b.first].pos;
+                      });
+            std::sort(keys.second.begin(), keys.second.end(),
+                      [&pop](const decltype(keys.second[0]) &a,
+                             const decltype(keys.second[0]) &b) {
+                          return pop.mutations[a.first].pos
+                                 < pop.mutations[b.first].pos;
+                      });
+            return (haplotype == true)
+                       ? fwdpp::haplotype_matrix(pop, individuals, keys.first,
+                                                 keys.second)
+                       : fwdpp::genotype_matrix(pop, individuals, keys.first,
+                                                keys.second);
+        }
+
+        template <typename poptype>
+        fwdpp::data_matrix
+        sample_random_individuals_details(const poptype &pop,
+                                          const GSLrng_t &rng,
+                                          const std::uint32_t nsam,
+                                          const bool haplotype) const
+        {
+            if (nsam > pop.N)
+                {
+                    throw std::invalid_argument(
+                        "sample size > population size");
+                }
+            std::vector<std::size_t> all_individuals(pop.N);
+            std::iota(all_individuals.begin(), all_individuals.end(), 0);
+            if (nsam == pop.N)
+                {
+                    return sample_individuals_details(pop, all_individuals,
+                                                      haplotype);
+                }
+
+            std::vector<std::size_t> individuals(nsam, 0);
+            gsl_ran_choose(rng.get(), individuals.data(), individuals.size(),
+                           all_individuals.data(), all_individuals.size(),
+                           sizeof(std::size_t));
+            return sample_individuals_details(pop, individuals, haplotype);
         }
     };
 } // namespace fwdpy11
