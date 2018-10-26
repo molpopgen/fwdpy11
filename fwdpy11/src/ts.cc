a6fccb86e59fb227b24c20c69571f875f7d3b9d6
molpopgen <krthornt@uci.edu>
2018-10-26 11:58:31 -0700

    Draft implementation on fwdpy11.ts.infinite_sites.

--- fwdpy11/src/ts.cc
+++ fwdpy11/src/ts.cc
@@ -8,9 +8,13 @@
 
 #include <fwdpp/ts/table_collection.hpp>
 #include <fwdpp/ts/table_simplifier.hpp>
+#include <fwdpp/ts/recycling.hpp>
+#include <fwdpp/ts/mutate_tables.hpp>
+#include <fwdpp/ts/count_mutations.hpp>
 
 #include <fwdpy11/types/Population.hpp>
 #include <fwdpy11/rng.hpp>
+#include <fwdpy11/policies/mutation.hpp>
 
 namespace py = pybind11;
 
@@ -49,6 +53,21 @@ simplify(const fwdpy11::Population& pop,
     fwdpp::ts::table_simplifier simplifier(pop.tables.genome_length());
     auto idmap = simplifier.simplify(t, samples, pop.mutations);
     return py::make_tuple(std::move(t), std::move(idmap));
+}
+
+inline std::size_t
+generate_neutral_variants(std::queue<std::size_t>& recycling_bin,
+                          fwdpy11::Population& pop,
+                          const fwdpy11::GSLrng_t& rng, const double left,
+                          const double right, const fwdpp::uint_t generation)
+{
+    const auto uniform = [left, right, &rng]() {
+        return gsl_ran_flat(rng.get(), left, right);
+    };
+    const auto return_zero = []() { return 0.0; };
+    return fwdpy11::infsites_Mutation(recycling_bin, pop.mutations,
+                                      pop.mut_lookup, generation, uniform,
+                                      return_zero, return_zero, 0);
 }
 
 PYBIND11_MODULE(ts, m)
@@ -158,9 +177,11 @@ PYBIND11_MODULE(ts, m)
     // TODO: work out conversion to msprime format
     // TODO: allow preserved_nodes to be cleared
     // TODO: allow access to the "right" member functions
-    py::class_<fwdpp::ts::table_collection>(m, "TableCollection",
-            "A table collection representing a succinct tree sequence.")
-        .def_property_readonly("L", &fwdpp::ts::table_collection::genome_length, "Genome length")
+    py::class_<fwdpp::ts::table_collection>(
+        m, "TableCollection",
+        "A table collection representing a succinct tree sequence.")
+        .def_property_readonly(
+            "L", &fwdpp::ts::table_collection::genome_length, "Genome length")
         .def_readonly("edges", &fwdpp::ts::table_collection::edge_table,
                       "The :class:`fwdpy11.ts.EdgeTable`.")
         .def_readonly("nodes", &fwdpp::ts::table_collection::node_table,
@@ -194,9 +215,31 @@ PYBIND11_MODULE(ts, m)
             )delim");
 
     m.def("infinite_sites",
-          [](const fwdpy11::GSLrng_t& rng, fwdpp::ts::table_collection& tables,
+          [](const fwdpy11::GSLrng_t& rng, fwdpy11::Population& pop,
              const std::vector<fwdpp::ts::TS_NODE_INT>& samples,
              const double mu) {
-              //TODO: implement
+              std::queue<std::size_t> recycling_bin
+                  = fwdpp::ts::make_mut_queue(
+                      pop.mcounts, pop.mcounts_from_preserved_nodes);
+              const auto apply_mutations
+                  = [&recycling_bin, &rng, &pop, &samples,
+                     mu](const double left, const double right,
+                         const fwdpp::uint_t generation) {
+                        return generate_neutral_variants(
+                            recycling_bin, pop, rng, left, right, generation);
+                    };
+              auto nmuts = fwdpp::ts::mutate_tables(rng, apply_mutations,
+                                                    pop.tables, samples, mu);
+              std::sort(pop.tables.mutation_table.begin(),
+                        pop.tables.mutation_table.end(),
+                        [&pop](const fwdpp::ts::mutation_record& a,
+                               const fwdpp::ts::mutation_record& b) {
+                            return pop.mutations[a.key].pos
+                                   < pop.mutations[b.key].pos;
+                        });
+              fwdpp::ts::count_mutations(pop.tables, pop.mutations, samples,
+                                         pop.mcounts,
+                                         pop.mcounts_from_preserved_nodes);
+              return nmuts;
           });
 }
