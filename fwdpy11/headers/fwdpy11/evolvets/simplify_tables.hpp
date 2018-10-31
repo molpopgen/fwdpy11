8ccda96ffcabf6dac442911c89cbf6234fdfbfca
molpopgen <krthornt@uci.edu>
2018-10-31 13:38:49 -0700

    Update behavior of simplify_tables to allow preservation of selected fixations.

--- fwdpy11/headers/fwdpy11/evolvets/simplify_tables.hpp
+++ fwdpy11/headers/fwdpy11/evolvets/simplify_tables.hpp
@@ -39,7 +39,9 @@ namespace fwdpy11
                     fwdpp::ts::table_collection &tables,
                     fwdpp::ts::table_simplifier &simplifier,
                     const fwdpp::ts::TS_NODE_INT first_sample_node,
-                    const std::size_t num_samples)
+                    const std::size_t num_samples,
+                    const bool preserve_selected_fixations,
+                    const bool simulating_neutral_variants)
     {
         tables.sort_tables(pop.mutations);
         std::vector<std::int32_t> samples(num_samples);
@@ -58,24 +60,35 @@ namespace fwdpy11
 #endif
         fwdpp::ts::count_mutations(tables, pop.mutations, samples, pop.mcounts,
                                    mcounts_from_preserved_nodes);
-        // TODO: the next several calls need to be updated
-        // for qtrait sims.
-        tables.mutation_table.erase(
-            std::remove_if(
-                tables.mutation_table.begin(), tables.mutation_table.end(),
-                [&pop, &mcounts_from_preserved_nodes](
-                    const fwdpp::ts::mutation_record &mr) {
-                    return pop.mcounts[mr.key] == 2 * pop.diploids.size()
-                           && mcounts_from_preserved_nodes[mr.key] == 0;
-                }),
-            tables.mutation_table.end());
+        // TODO: update this to allow neutral mutations to be simulated
+        // TODO: better fixation handling via accounting for number of ancient samples
+        if (!preserve_selected_fixations && !simulating_neutral_variants)
+            {
+                tables.mutation_table.erase(
+                    std::remove_if(
+                        tables.mutation_table.begin(),
+                        tables.mutation_table.end(),
+                        [&pop, &mcounts_from_preserved_nodes](
+                            const fwdpp::ts::mutation_record &mr) {
+                            return pop.mcounts[mr.key]
+                                       == 2 * pop.diploids.size()
+                                   && mcounts_from_preserved_nodes[mr.key]
+                                          == 0;
+                        }),
+                    tables.mutation_table.end());
+            }
         fwdpp::ts::remove_fixations_from_gametes(
             pop.gametes, pop.mutations, pop.mcounts,
-            mcounts_from_preserved_nodes, 2 * pop.diploids.size());
+            mcounts_from_preserved_nodes, 2 * pop.diploids.size(),
+            preserve_selected_fixations);
 
-        fwdpp::ts::flag_mutations_for_recycling(
-            pop.mutations, pop.mcounts, mcounts_from_preserved_nodes,
-            pop.mut_lookup, 2 * pop.diploids.size());
+        if (!preserve_selected_fixations && !simulating_neutral_variants)
+            {
+                fwdpp::ts::flag_mutations_for_recycling(
+                    pop.mutations, pop.mcounts, mcounts_from_preserved_nodes,
+                    pop.mut_lookup, 2 * pop.diploids.size(),
+                    preserve_selected_fixations);
+            }
         //confirm_mutation_counts(pop, tables);
         return idmap;
     }
