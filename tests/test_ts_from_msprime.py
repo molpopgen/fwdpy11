5b51ddde178f678a71d884ac835e32a18d948000
molpopgen <krthornt@uci.edu>
2018-11-02 11:25:42 -0700

    update test to new function name

--- tests/test_ts_from_msprime.py
+++ tests/test_ts_from_msprime.py
@@ -11,14 +11,14 @@ class testConversion(unittest.TestCase):
         self.ts = msprime.simulate(10, recombination_rate=0.025, Ne=1000)
 
     def testGetTables(self):
-        n, e, ntips, l = fwdpy11.ts_from_msprime.convert_tables(self.ts, False)
+        n, e, ntips, l = fwdpy11.ts_from_msprime._convert_tables(self.ts, False)
         self.assertEqual(ntips, 10)
         self.assertEqual(l, 1.0)
         na = np.array(n, copy=False)
         self.assertTrue(np.all(na['time'][:10] == 0))
 
     def testGetTablesDiscretizeTime(self):
-        n, e, ntips, l = fwdpy11.ts_from_msprime.convert_tables(self.ts, True)
+        n, e, ntips, l = fwdpy11.ts_from_msprime._convert_tables(self.ts, True)
         self.assertEqual(ntips, 10)
         self.assertEqual(l, 1.0)
         na = np.array(n, copy=False)
