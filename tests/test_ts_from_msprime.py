50d747cab89bc6eea74e32aa720f00780fd62601
molpopgen <krthornt@uci.edu>
2018-11-05 18:48:02 -0800

    update unit test

--- tests/test_ts_from_msprime.py
+++ tests/test_ts_from_msprime.py
@@ -11,24 +11,24 @@ class testConversion(unittest.TestCase):
         self.ts = msprime.simulate(10, recombination_rate=0.025, Ne=1000)
 
     def testGetTables(self):
-        n, e, ntips, l = fwdpy11.ts_from_msprime._convert_tables(self.ts, False)
+        n, e, ntips, l = fwdpy11.ts_from_msprime._convert_tables(self.ts)
         self.assertEqual(ntips, 10)
         self.assertEqual(l, 1.0)
         na = np.array(n, copy=False)
         self.assertTrue(np.all(na['time'][:10] == 0))
 
-    def testGetTablesDiscretizeTime(self):
-        n, e, ntips, l = fwdpy11.ts_from_msprime._convert_tables(self.ts, True)
-        self.assertEqual(ntips, 10)
-        self.assertEqual(l, 1.0)
-        na = np.array(n, copy=False)
-        tzero = np.where(na['time'] == 0.0)
-        self.assertTrue(len(tzero[0]) == 10)
+    # def testGetTablesDiscretizeTime(self):
+    #     n, e, ntips, l = fwdpy11.ts_from_msprime._convert_tables(self.ts)
+    #     self.assertEqual(ntips, 10)
+    #     self.assertEqual(l, 1.0)
+    #     na = np.array(n, copy=False)
+    #     tzero = np.where(na['time'] == 0.0)
+    #     self.assertTrue(len(tzero[0]) == 10)
 
     def testCreateSlocusPop(self):
-        pop = fwdpy11.SlocusPop.create_from_msprime(self.ts, True)
-        self.assertEqual(pop.N, 5);
-        self.assertEqual(pop.tables.genome_length(),1.0)
+        pop = fwdpy11.SlocusPop.create_from_msprime(self.ts)
+        self.assertEqual(pop.N, 5)
+        self.assertEqual(pop.tables.genome_length(), 1.0)
 
 
 if __name__ == "__main__":
