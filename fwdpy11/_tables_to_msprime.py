6f90ddc364f45c96f7936a6caa6f31d69c1e7f60
molpopgen <krthornt@uci.edu>
2018-11-05 17:34:13 -0800

    Add mutation metadata

--- fwdpy11/_tables_to_msprime.py
+++ fwdpy11/_tables_to_msprime.py
@@ -1,5 +1,22 @@
 import numpy as np
 import msprime
+import pickle
+
+
+# TODO: mutation origin times are recorded forwards in time,
+# which is likely confusing, so we omit them
+def _generate_mutation_metadata(pop):
+    muts = []
+    for mr in pop.tables.mutations:
+        m = pop.mutations[mr.key]
+        d = {'s': m.s,
+             'h': m.h,
+             # 'g': m.g,
+             'label': m.label,
+             'esizes': list(m.esizes),
+             'heffects': list(m.heffects)}
+        muts.append(str(d).encode('utf-8'))
+    return msprime.pack_bytes(muts)
 
 
 def dump_tables_to_msprime(pop):
@@ -25,8 +42,11 @@ def dump_tables_to_msprime(pop):
                          ancestral_state_offset=ancestral_state_offset)
 
     derived_state = np.zeros(len(mut_view), dtype=np.int8)+ord('1')
+    md, mdo = _generate_mutation_metadata(pop)
     tc.mutations.set_columns(site=np.arange(len(mpos), dtype=np.int32),
                              node=mut_view['node'],
                              derived_state=derived_state,
-                             derived_state_offset=ancestral_state_offset)
+                             derived_state_offset=ancestral_state_offset,
+                             metadata=md,
+                             metadata_offset=mdo)
     return tc.tree_sequence()
