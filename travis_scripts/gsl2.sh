05dd20c3fca68db3c608cc7c0ca6cdeb72ffdc1f
molpopgen <krthornt@uci.edu>
2018-11-16 10:09:11 -0800

    use 2 jobs to build GSL and send stdout to dev/null

--- travis_scripts/gsl2.sh
+++ travis_scripts/gsl2.sh
@@ -4,6 +4,6 @@ wget http://mirror.us-midwest-1.nexcess.net/gnu/gsl/gsl-2.2.tar.gz
 tar xzf gsl-2.2.tar.gz
 cd gsl-2.2
 ./configure
-make
+make -j 2 > /dev/null 
 sudo make install > /dev/null 2> /dev/null
 cd ..
