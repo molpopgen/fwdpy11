eba47eb33ce051a8a18e5de97c87cf0788e15834
molpopgen <krthornt@uci.edu>
2018-11-16 10:01:08 -0800

    install pybind11 manuall on travis/ubuntu

--- travis_scripts/installation_script.sh
+++ travis_scripts/installation_script.sh
@@ -36,4 +36,10 @@ else
     sudo apt-get -f install python-dev libffi-dev libssl-dev libhdf5-dev cmake
     ./travis_scripts/gsl2.sh
     pip install -r requirements.txt
+
+    # We need to get pybind11 NOT from pip
+    curl -L https://github.com/pybind/pybind11/archive/v2.2.4.tar.gz > pybind11-2.2.4.tar.gz
+    tar xzf pybind11-2.2.4.tar.gz
+    # Skip running the pybind11 unit suite--not needed...
+    cd pybind11-2.2.4.tar.gz && cmake . -DPYBIND11_TEST=0 && sudo make install && cd ..
 fi
