167039d1f7a7c6ca98fc494e6c9ca6aec48dc7ed
molpopgen <krthornt@uci.edu>
2019-01-01 15:50:19 -0800

    Init ExpS in _fwdpp

--- fwdpy11/src/_fwdpy11.cc
+++ fwdpy11/src/_fwdpy11.cc
@@ -6,6 +6,7 @@ void init_Region(py::module &);
 void init_Sregion(py::module &);
 void init_GammaS(py::module &);
 void init_ConstantS(py::module &);
+void init_ExpS(py::module &);
 
 PYBIND11_MODULE(_fwdpy11, m)
 {
@@ -14,4 +15,5 @@ PYBIND11_MODULE(_fwdpy11, m)
     init_Sregion(m);
     init_GammaS(m);
     init_ConstantS(m);
+    init_ExpS(m);
 }
