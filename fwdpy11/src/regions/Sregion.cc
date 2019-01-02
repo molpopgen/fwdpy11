0f5f196b58a9a1c1b6b92779f31704f42c3e4c43
molpopgen <krthornt@uci.edu>
2019-01-02 09:53:38 -0800

    Change field names to match existing API exactly.

--- fwdpy11/src/regions/Sregion.cc
+++ fwdpy11/src/regions/Sregion.cc
@@ -8,13 +8,15 @@ init_Sregion(py::module& m)
 {
     py::class_<fwdpy11::Sregion>(m, "Sregion")
         .def_property_readonly(
-            "beg", [](const fwdpy11::Sregion& s) { return s.beg(); })
+            "b", [](const fwdpy11::Sregion& s) { return s.beg(); })
         .def_property_readonly(
-            "end", [](const fwdpy11::Sregion& s) { return s.end(); })
+            "e", [](const fwdpy11::Sregion& s) { return s.end(); })
         .def_property_readonly(
-            "weight", [](const fwdpy11::Sregion& s) { return s.weight(); })
+            "w", [](const fwdpy11::Sregion& s) { return s.weight(); })
         .def_property_readonly(
-            "label", [](const fwdpy11::Sregion& s) { return s.label(); })
+            "l", [](const fwdpy11::Sregion& s) { return s.label(); })
+        .def_property_readonly(
+            "c", [](const fwdpy11::Sregion& s) { return s.region.coupled; })
         .def_readonly("scaling", &fwdpy11::Sregion::scaling);
 }
 
