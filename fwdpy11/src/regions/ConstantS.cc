7124a707b9455c6b8f645d20b731d85d71ce4c1e
molpopgen <krthornt@uci.edu>
2019-01-01 16:09:06 -0800

    Change ConstantS to be API-compatible with current fwdpy11

--- fwdpy11/src/regions/ConstantS.cc
+++ fwdpy11/src/regions/ConstantS.cc
@@ -10,9 +10,10 @@ init_ConstantS(py::module& m)
         .def(py::init<double, double, double, double, double, bool,
                       std::uint16_t, double>(),
              py::arg("beg"), py::arg("end"), py::arg("weight"),
-             py::arg("esize"), py::arg("h") = 1.0, py::arg("coupled") = true,
+             py::arg("s"), py::arg("h") = 1.0, py::arg("coupled") = true,
              py::arg("label") = 0, py::arg("scaling") = 1.0)
         .def_readonly("esize", &fwdpy11::ConstantS::esize)
+        .def_readonly("s", &fwdpy11::ConstantS::esize)
         .def_readonly("h", &fwdpy11::ConstantS::dominance);
 }
 
