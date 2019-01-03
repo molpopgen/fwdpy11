Fix MultivariateGaussianEffects constructor to allow input matrix to  be either covariance or the Cholesky decomposition output. Add fixed effect field to MultivariateGaussianEffects and add default arguments to the Python class.

tests/test_regions.py
@@ -110,7 +110,7 @@ class testMutationRegions(unittest.TestCase):
 class testMultivariateGaussianEffects(unittest.TestCase):
     def testConstruction(self):
         fwdpy11.MultivariateGaussianEffects(
-            0, 1, 1, np.identity(2), 1.0, True, 0)
+            0, 1, 1, np.identity(2))
 
 
 class testMlocusMutationRegions(unittest.TestCase):