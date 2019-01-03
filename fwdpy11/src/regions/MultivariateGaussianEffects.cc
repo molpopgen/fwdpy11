93fe47464d9ab04c75e37eb496a587a580d7e4fe
molpopgen <krthornt@uci.edu>
2019-01-02 17:58:50 -0800

    Draft implementation of Python constructor for MultivariateGaussianEffects.

--- fwdpy11/src/regions/MultivariateGaussianEffects.cc
+++ fwdpy11/src/regions/MultivariateGaussianEffects.cc
@@ -1,4 +1,5 @@
 #include <pybind11/pybind11.h>
+#include <pybind11/numpy.h>
 #include <fwdpy11/regions/MultivariateGaussianEffects.hpp>
 
 namespace py = pybind11;
@@ -7,5 +8,18 @@ void
 init_MultivariateGaussianEffects(py::module& m)
 {
     py::class_<fwdpy11::MultivariateGaussianEffects, fwdpy11::Sregion>(
-        m, "MultivariateGaussianEffects");
+        m, "MultivariateGaussianEffects")
+        .def(py::init([](double beg, double end, double weight, bool coupled,
+                         py::array_t<double> cov_matrix, double h,
+                         std::uint16_t label) {
+            auto r = cov_matrix.unchecked<2>();
+            if (r.shape(0) != r.shape(1))
+                {
+                    throw std::invalid_argument("input matrix is not square");
+                }
+            gsl_matrix_const_view v = gsl_matrix_const_view_array(
+                r.data(0,0), r.shape(0), r.shape(1));
+            return fwdpy11::MultivariateGaussianEffects(beg, end, weight,
+                                                        coupled, v, h, label);
+        }));
 }
