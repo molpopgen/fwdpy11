691acd19da5cb8148de8c91c1c4993bee96361da
molpopgen <krthornt@uci.edu>
2019-01-02 18:49:22 -0800

    Add tests for invalid input matrix

--- tests/test_regions.py
+++ tests/test_regions.py
@@ -112,6 +112,20 @@ class testMultivariateGaussianEffects(unittest.TestCase):
         fwdpy11.MultivariateGaussianEffects(
             0, 1, 1, np.identity(2))
 
+    def testInvalidMatrixShape(self):
+        # Input matrix has wrong shape:
+        m = np.array([-1.0] * 4)
+        with self.assertRaises(ValueError):
+            fwdpy11.MultivariateGaussianEffects(
+                0, 1, 1, m)
+
+    def testInvalidMatrix(self):
+        # This is not a positive-definite covariance
+        # matrix:
+        m = np.array([-1.0] * 4).reshape((2,2))
+        with self.assertRaises(ValueError):
+            fwdpy11.MultivariateGaussianEffects(
+                0, 1, 1, m)
 
 class testMlocusMutationRegions(unittest.TestCase):
     @classmethod
