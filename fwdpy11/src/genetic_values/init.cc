f6fc2ea0b39e83497fbdc1017d3c1c7ba36c76ff
molpopgen <krthornt@uci.edu>
2019-01-03 17:09:13 -0800

    update init function for genetic values

--- fwdpy11/src/genetic_values/init.cc
+++ fwdpy11/src/genetic_values/init.cc
@@ -2,12 +2,16 @@
 
 namespace py = pybind11;
 
+void init_MultivariateGeneticValueToFitnessMap(py::module&);
+void init_MultivariateGSS(py::module&);
 void init_SlocusPopMultivariateGeneticValueWithMapping(py::module&);
 void init_SlocusMultivariateEffectsStrictAdditive(py::module&);
 
 void
 init_genetic_values(py::module& m)
 {
+    init_MultivariateGeneticValueToFitnessMap(m);
+    init_MultivariateGSS(m);
     init_SlocusPopMultivariateGeneticValueWithMapping(m);
     init_SlocusMultivariateEffectsStrictAdditive(m);
 }
