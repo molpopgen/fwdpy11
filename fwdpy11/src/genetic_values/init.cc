92b33d8903b3050ce5019e0a997626b2b99c014d
molpopgen <krthornt@uci.edu>
2019-01-05 03:44:32 -0800

    reorg genetic_values/init.cc so that the logic of the order of calls is clear

--- fwdpy11/src/genetic_values/init.cc
+++ fwdpy11/src/genetic_values/init.cc
@@ -9,12 +9,30 @@ void init_SlocusPopMultivariateGeneticValueWithMapping(py::module&);
 void init_SlocusMultivariateEffectsStrictAdditive(py::module&);
 
 void
-init_genetic_values(py::module& m)
+init_base_classes(py::module& m)
 {
     init_MultivariateGeneticValueToFitnessMap(m);
+    init_SlocusPopMultivariateGeneticValueWithMapping(m);
+}
+
+void
+init_gvalue_to_fitness_classes(py::module& m)
+{
     init_MultivariateGSS(m);
     init_MultivariateGSSmo(m);
-    init_SlocusPopMultivariateGeneticValueWithMapping(m);
+}
+
+void
+init_genetic_value_classes(py::module& m)
+{
     init_SlocusMultivariateEffectsStrictAdditive(m);
+}
+
+void
+init_genetic_values(py::module& m)
+{
+    init_base_classes(m);
+    init_gvalue_to_fitness_classes(m);
+    init_genetic_value_classes(m);
 }
 
