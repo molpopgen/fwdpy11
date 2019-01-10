367d2aed5ae04ec576d279f08f91532f31c3f9e6
molpopgen <krthornt@uci.edu>
2019-01-10 15:32:20 -0800

    Add other marginal tree fields to new visitor

--- fwdpy11/src/ts.cc
+++ fwdpy11/src/ts.cc
@@ -166,13 +166,24 @@ struct VariantIterator
 struct tree_visitor_wrapper
 {
     fwdpp::ts::tree_visitor visitor;
-    py::array parents;
-    py::array leaf_counts;
+    py::array parents, leaf_counts, preserved_leaf_counts, left_sib, right_sib,
+        left_child, right_child, left_sample, right_sample, next_sample,
+        sample_index_map;
     tree_visitor_wrapper(const fwdpp::ts::table_collection& tables,
                          const std::vector<fwdpp::ts::TS_NODE_INT>& samples)
         : visitor(tables, samples),
           parents(make_1d_ndarray(visitor.tree().parents)),
-          leaf_counts(make_1d_ndarray(visitor.tree().leaf_counts))
+          leaf_counts(make_1d_ndarray(visitor.tree().leaf_counts)),
+          preserved_leaf_counts(
+              make_1d_ndarray(visitor.tree().preserved_leaf_counts)),
+          left_sib(make_1d_ndarray(visitor.tree().left_sib)),
+          right_sib(make_1d_ndarray(visitor.tree().right_sib)),
+          left_child(make_1d_ndarray(visitor.tree().left_child)),
+          right_child(make_1d_ndarray(visitor.tree().right_child)),
+          left_sample(make_1d_ndarray(visitor.tree().left_sample)),
+          right_sample(make_1d_ndarray(visitor.tree().right_sample)),
+          next_sample(make_1d_ndarray(visitor.tree().next_sample)),
+          sample_index_map(make_1d_ndarray(visitor.tree().sample_index_map))
     {
     }
 
@@ -409,13 +420,30 @@ PYBIND11_MODULE(ts, m)
              py::arg("tables"), py::arg("samples"))
         .def_readwrite("parents", &tree_visitor_wrapper::parents)
         .def_readonly("leaf_counts", &tree_visitor_wrapper::leaf_counts)
-        .def_property_readonly("left",[](const tree_visitor_wrapper&self){return self.visitor.tree().left;})
-        .def_property_readonly("right",[](const tree_visitor_wrapper&self){return self.visitor.tree().right;})
-        .def("__call__",&tree_visitor_wrapper::operator())
+        .def_readonly("preserved_leaf_counts",
+                      &tree_visitor_wrapper::preserved_leaf_counts)
+        .def_readonly("left_sib", &tree_visitor_wrapper::left_sib)
+        .def_readonly("right_sib", &tree_visitor_wrapper::right_sib)
+        .def_readonly("left_child", &tree_visitor_wrapper::left_child)
+        .def_readonly("right_child", &tree_visitor_wrapper::right_child)
+        .def_readonly("left_sample", &tree_visitor_wrapper::leaf_counts)
+        .def_readonly("right_sample", &tree_visitor_wrapper::leaf_counts)
+        .def_readonly("next_sample", &tree_visitor_wrapper::next_sample)
+        .def_readonly("sample_index_map",
+                      &tree_visitor_wrapper::sample_index_map)
+        .def_property_readonly("left",
+                               [](const tree_visitor_wrapper& self) {
+                                   return self.visitor.tree().left;
+                               })
+        .def_property_readonly("right",
+                               [](const tree_visitor_wrapper& self) {
+                                   return self.visitor.tree().right;
+                               })
+        .def("__call__", &tree_visitor_wrapper::operator())
         .def("total_time",
              [](const tree_visitor_wrapper& self,
                 const fwdpp::ts::node_vector& nodes) {
-             const auto & m = self.visitor.tree();
+                 const auto& m = self.visitor.tree();
                  if (m.parents.size() != nodes.size())
                      {
                          throw std::invalid_argument(
