8267d83fcb77649bbe5203992aa86c58129a3644
molpopgen <krthornt@uci.edu>
2019-02-15 08:41:49 +0100

    count_mutations now returns list of (key, count) for the sample. Add test for count_mutations.

--- fwdpy11/src/ts/count_mutations.cc
+++ fwdpy11/src/ts/count_mutations.cc
@@ -16,7 +16,26 @@ init_count_mutations(py::module& m)
               mc.resize(pop.mutations.size());
               fwdpp::ts::count_mutations(pop.tables, pop.mutations, samples,
                                          mc);
-              return mc;
-          });
+              std::vector<std::pair<std::size_t, fwdpp::uint_t>> rv;
+              for (std::size_t i = 0; i < mc.size(); ++i)
+                  {
+                      if (mc[i] > 0)
+                          {
+                              rv.emplace_back(i, mc[i]);
+                          }
+                  }
+              return rv;
+          },
+          R"delim(
+          Count mutation occurrences in a sample of nodes.
+
+          :param pop: A population
+          :type pop: :class:`fwdpy11.Population`
+          :param samples: List of samples
+          :type samples: list
+
+          :return: List of (key, count) tuples.
+          :rtype: list
+          )delim");
 }
 
