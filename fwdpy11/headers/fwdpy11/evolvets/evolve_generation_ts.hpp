efe591c267f7828765919a18e643260717e6e8c0
molpopgen <krthornt@uci.edu>
2019-02-16 08:50:39 +0100

    Copy updated fwdpp version of evolving with tree sequences, creating a new version of evolve_generation_ts. Rename the current/old version with _old. Minimal modifications made to get new version to compile.

--- fwdpy11/headers/fwdpy11/evolvets/evolve_generation_ts.hpp
+++ fwdpy11/headers/fwdpy11/evolvets/evolve_generation_ts.hpp
@@ -29,6 +29,7 @@
 
 #include <fwdpp/util.hpp>
 #include <fwdpp/mutate_recombine.hpp>
+#include <fwdpp/ts/generate_offspring.hpp>
 #include <fwdpp/ts/get_parent_ids.hpp>
 #include <fwdpp/ts/table_collection.hpp>
 #include <fwdpp/ts/table_simplifier.hpp>
@@ -36,11 +37,92 @@
 namespace fwdpy11
 {
 
+    template <typename poptype, typename rng_t, typename genetic_param_holder>
+    std::pair<fwdpp::ts::mut_rec_intermediates,
+              fwdpp::ts::mut_rec_intermediates>
+    generate_offspring(
+        const rng_t& rng,
+        const std::pair<std::size_t, std::size_t> parent_indexes, poptype& pop,
+        typename poptype::diploid_t& offspring, genetic_param_holder& genetics)
+    {
+        auto offspring_data = fwdpp::ts::generate_offspring(
+            rng.get(), parent_indexes, fwdpp::ts::selected_variants_only(),
+            pop, genetics, offspring);
+#ifndef NDEBUG
+        for (auto& m : offspring_data.first.mutation_keys)
+            {
+                auto itr = pop.mut_lookup.equal_range(pop.mutations[m].pos);
+                assert(std::distance(itr.first, itr.second) == 1);
+            }
+        for (auto& m : offspring_data.second.mutation_keys)
+            {
+                auto itr = pop.mut_lookup.equal_range(pop.mutations[m].pos);
+                assert(std::distance(itr.first, itr.second) == 1);
+            }
+#endif
+        return offspring_data;
+    }
+
+    template <typename rng_t, typename poptype, typename pick_parent1_fxn,
+              typename pick_parent2_fxn, typename offspring_metadata_fxn,
+              typename genetic_param_holder>
+    void
+    evolve_generation_ts(
+        const rng_t& rng, poptype& pop, genetic_param_holder& genetics,
+        const fwdpp::uint_t N_next, const pick_parent1_fxn& pick1,
+        const pick_parent2_fxn& pick2,
+        const offspring_metadata_fxn& update_offspring,
+        const fwdpp::uint_t generation, fwdpp::ts::table_collection& tables,
+        std::int32_t first_parental_index, std::int32_t next_index)
+    {
+        fwdpp::debug::all_gametes_extant(pop);
+
+        genetics.gamete_recycling_bin = fwdpp::make_gamete_queue(pop.gametes);
+
+        fwdpp::zero_out_gametes(pop);
+
+        decltype(pop.diploids) offspring(N_next);
+        decltype(pop.diploid_metadata) offspring_metadata(N_next);
+
+        // Generate the offspring
+        auto next_index_local = next_index;
+        for (std::size_t next_offspring = 0; next_offspring < offspring.size();
+             ++next_offspring)
+            {
+                auto p1 = pick1();
+                auto p2 = pick2(p1);
+                auto& dip = offspring[next_offspring];
+                auto offspring_data = generate_offspring(
+                    rng, std::make_pair(p1, p2), pop, dip, genetics);
+                auto p1id = fwdpp::ts::get_parent_ids(
+                    first_parental_index, p1, offspring_data.first.swapped);
+                auto p2id = fwdpp::ts::get_parent_ids(
+                    first_parental_index, p2, offspring_data.second.swapped);
+                tables.add_offspring_data(
+                    next_index_local++, offspring_data.first.breakpoints,
+                    offspring_data.first.mutation_keys, p1id, 0, generation);
+                tables.add_offspring_data(
+                    next_index_local++, offspring_data.second.breakpoints,
+                    offspring_data.second.mutation_keys, p2id, 0, generation);
+
+                // Give the caller a chance to generate
+                // any metadata for the offspring that
+                // may depend on the parents
+                offspring_metadata[next_offspring].label = next_offspring;
+                update_offspring(offspring_metadata[next_offspring], p1, p2,
+                                 pop.diploid_metadata);
+            }
+        assert(next_index_local
+               == next_index + 2 * static_cast<std::int32_t>(N_next));
+        // This is constant-time
+        pop.diploids.swap(offspring);
+    }
+
     // Wow, that's a lot of stuff needed:
     template <typename poptype, typename rng_t, typename breakpoint_function,
               typename mutation_model, typename mrecbin, typename grecbin>
     std::int32_t
-    generate_offspring(
+    generate_offspring_old(
         const rng_t& rng, const breakpoint_function& recmodel,
         const mutation_model& mmodel, const double mu,
         const std::size_t parent, const fwdpp::uint_t parent_g1,
@@ -82,17 +164,15 @@ namespace fwdpy11
               typename pick_parent2_fxn, typename offspring_metadata_fxn,
               typename breakpoint_function, typename mutation_model>
     void
-    evolve_generation_ts(const rng_t& rng, poptype& pop,
-                         const fwdpp::uint_t N_next, const double mu,
-                         const pick_parent1_fxn& pick1,
-                         const pick_parent2_fxn& pick2,
-                         const offspring_metadata_fxn& update_offspring,
-                         const mutation_model& mmodel,
-                         fwdpp::flagged_mutation_queue& mutation_recycling_bin,
-                         const breakpoint_function& recmodel,
-                         const fwdpp::uint_t generation,
-                         std::int32_t first_parental_index,
-                         std::int32_t next_index)
+    evolve_generation_ts_old(
+        const rng_t& rng, poptype& pop, const fwdpp::uint_t N_next,
+        const double mu, const pick_parent1_fxn& pick1,
+        const pick_parent2_fxn& pick2,
+        const offspring_metadata_fxn& update_offspring,
+        const mutation_model& mmodel,
+        fwdpp::flagged_mutation_queue& mutation_recycling_bin,
+        const breakpoint_function& recmodel, const fwdpp::uint_t generation,
+        std::int32_t first_parental_index, std::int32_t next_index)
     {
 
         auto gamete_recycling_bin = fwdpp::make_gamete_queue(pop.gametes);
