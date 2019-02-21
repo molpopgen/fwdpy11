405b49f6ca6aee5680fc70203eef5664f77e31f3
molpopgen <krthornt@uci.edu>
2019-02-21 07:18:12 -0800

    Add GeneticMap base class and update evolution function for SlocusPop to take a const reference to the base.

--- fwdpy11/src/regions/RecombinationRegions.cc
+++ fwdpy11/src/regions/RecombinationRegions.cc
@@ -7,7 +7,10 @@ namespace py = pybind11;
 void
 init_RecombinationRegions(py::module& m)
 {
-    py::class_<fwdpy11::RecombinationRegions>(m, "RecombinationRegions")
+    py::class_<fwdpy11::GeneticMap>(m, "GeneticMap", "ABC for genetic maps");
+
+    py::class_<fwdpy11::RecombinationRegions, fwdpy11::GeneticMap>(
+        m, "RecombinationRegions")
         .def(py::init<double, std::vector<fwdpy11::Region>>())
         .def_readonly("weights", &fwdpy11::RecombinationRegions::weights);
 
