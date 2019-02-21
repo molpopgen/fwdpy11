5398c54c0a8a101255f78560c05f244571c8febf
molpopgen <krthornt@uci.edu>
2019-02-21 11:07:22 -0800

    Remove unused argument from WFSlocusPop_ts

--- fwdpy11/src/tsevolution/slocuspop.cc
+++ fwdpy11/src/tsevolution/slocuspop.cc
@@ -49,7 +49,7 @@ wfSlocusPop_ts(
     const fwdpy11::GSLrng_t &rng, fwdpy11::SlocusPop &pop,
     fwdpy11::samplerecorder &sr, const unsigned simplification_interval,
     py::array_t<std::uint32_t> popsizes, //const double mu_neutral,
-    const double mu_selected, const double recrate,
+    const double mu_selected, 
     const fwdpy11::MutationRegions &mmodel,
     const fwdpy11::GeneticMap &rmodel,
     fwdpy11::SlocusPopGeneticValue &genetic_value_fxn,
