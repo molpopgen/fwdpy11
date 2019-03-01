32a0e99337aa73217bb9ad34ef199c519241f7cc
molpopgen <krthornt@uci.edu>
2019-03-01 11:55:27 -0800

    update unit test to new module and function names

--- tests/test_ts_from_msprime.py
+++ tests/test_ts_from_msprime.py
@@ -1,5 +1,5 @@
 import unittest
-import fwdpy11.ts_from_msprime
+import fwdpy11.ts_from_tskit
 import numpy as np
 import fwdpy11
 
@@ -11,7 +11,7 @@ class testConversion(unittest.TestCase):
         self.ts = msprime.simulate(10, recombination_rate=0.025, Ne=1000)
 
     def testGetTables(self):
-        n, e, ntips, l = fwdpy11.ts_from_msprime._convert_tables(self.ts)
+        n, e, ntips, l = fwdpy11.ts_from_tskit._convert_tables(self.ts)
         self.assertEqual(ntips, 10)
         self.assertEqual(l, 1.0)
         na = np.array(n, copy=False)
@@ -26,7 +26,7 @@ class testConversion(unittest.TestCase):
     #     self.assertTrue(len(tzero[0]) == 10)
 
     def testCreateSlocusPop(self):
-        pop = fwdpy11.SlocusPop.create_from_msprime(self.ts)
+        pop = fwdpy11.SlocusPop.create_from_tskit(self.ts)
         self.assertEqual(pop.N, 5)
         self.assertEqual(pop.tables.genome_length(), 1.0)
 
