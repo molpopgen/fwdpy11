d6db19ed4473e84596248f295e40ac5e953f622f
molpopgen <krthornt@uci.edu>
2019-03-07 14:33:08 -0800

    Fix passing of prune_selected to C++ function. Fixes #200.

--- fwdpy11/wright_fisher_ts.py
+++ fwdpy11/wright_fisher_ts.py
@@ -90,11 +90,12 @@ def evolve(rng, pop, params, simplification_interval, recorder=None,
 
         from fwdpy11.tsrecorders import SampleRecorder
         sr = SampleRecorder()
+        print(params.prune_selected)
         WFSlocusPop_ts(rng, pop, sr, simplification_interval,
                        params.demography, params.mutrate_s,
                        mm, rm, params.gvalue,
                        recorder, stopping_criterion,
-                       params.pself, params.prune_selected is True,
+                       params.pself, params.prune_selected is False,
                        suppress_table_indexing, record_gvalue_matrix,
                        track_mutation_counts)
     else:
@@ -132,6 +133,6 @@ def evolve(rng, pop, params, simplification_interval, recorder=None,
                        mm, rm,
                        params.interlocus_rec, params.gvalue,
                        recorder, stopping_criterion,
-                       params.pself, params.prune_selected is True,
+                       params.pself, params.prune_selected is False,
                        suppress_table_indexing, record_gvalue_matrix,
                        track_mutation_counts)
