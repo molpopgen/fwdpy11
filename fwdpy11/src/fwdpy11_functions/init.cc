80cb8854d9e331361a74eaedf901e7fc29f1e530
molpopgen <krthornt@uci.edu>
2019-03-19 12:13:44 -0700

    Fix initialization of sort_gamete_keys

--- fwdpy11/src/fwdpy11_functions/init.cc
+++ fwdpy11/src/fwdpy11_functions/init.cc
@@ -3,8 +3,10 @@
 namespace py = pybind11;
 
 void init_change_effect_size(py::module &);
+void init_sort_gamete_keys(py::module &);
 
 void initialize_fwdpy11_functions(py::module & m)
 {
     init_change_effect_size(m);
+    init_sort_gamete_keys(m);
 }
