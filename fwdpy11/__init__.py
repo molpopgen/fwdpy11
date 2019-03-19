7d71f09b4e120beacb97f559ba64aa746b67587f
molpopgen <krthornt@uci.edu>
2019-03-19 11:21:26 -0700

    Move methods for setting GSL error handler to _fwdpy11.cc

--- fwdpy11/__init__.py
+++ fwdpy11/__init__.py
@@ -22,7 +22,6 @@ import sys
 if sys.version_info[0] < 3:
     raise ValueError("Python3 required!")
 
-from fwdpy11._init import * # NOQA
 from fwdpy11._version import __version__ # NOQA
 from ._fwdpy11 import * # NOQA
 
