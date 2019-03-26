d435983231ab004d8def0c2f7c9d573b4033c7c6
molpopgen <krthornt@uci.edu>
2019-03-26 09:34:44 -0700

    Add wrapper functions to return readonly arrays.

--- fwdpy11/headers/fwdpy11/numpy/array.hpp
+++ fwdpy11/headers/fwdpy11/numpy/array.hpp
@@ -20,6 +20,18 @@ namespace fwdpy11
 
     template <typename T>
     inline pybind11::array_t<T>
+    make_1d_ndarray_readonly(const std::vector<T>& v)
+    // Returns a readonly 1d numpy array that does not own
+    // its data.
+    {
+        auto rv = pybind11::array_t<T>({ v.size() }, { sizeof(T) }, v.data(),
+                                       pybind11::cast(v));
+        rv.attr("flags").attr("writeable") = false;
+        return rv;
+    }
+
+    template <typename T>
+    inline pybind11::array_t<T>
     make_2d_ndarray(const std::vector<T>& v, std::size_t dim1,
                     std::size_t dim2)
     // Returns a 2d numpy array that does not own
@@ -27,6 +39,19 @@ namespace fwdpy11
     {
         auto rv = pybind11::array_t<T>({ dim1, dim2 }, v.data(),
                                        pybind11::cast(v));
+        return rv;
+    }
+
+    template <typename T>
+    inline pybind11::array_t<T>
+    make_2d_ndarray_readonly(const std::vector<T>& v, std::size_t dim1,
+                             std::size_t dim2)
+    // Returns a 2d numpy array that does not own
+    // its data.
+    {
+        auto rv = pybind11::array_t<T>({ dim1, dim2 }, v.data(),
+                                       pybind11::cast(v));
+        rv.attr("flags").attr("writeable") = false;
         return rv;
     }
 } // namespace fwdpy11
