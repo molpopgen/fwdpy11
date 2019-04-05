189a67e93baea6bb848af3ab317e9ac3fb652e56
molpopgen <krthornt@uci.edu>
2019-04-05 05:24:14 -0700

    Add Sregion::pickle as pure virtual fxn.

--- fwdpy11/headers/fwdpy11/regions/Sregion.hpp
+++ fwdpy11/headers/fwdpy11/regions/Sregion.hpp
@@ -54,6 +54,7 @@ namespace fwdpy11
 
         virtual std::unique_ptr<Sregion> clone() const = 0;
         virtual std::string repr() const = 0;
+        virtual pybind11::tuple pickle() const = 0;
         virtual std::uint32_t operator()(
             fwdpp::flagged_mutation_queue& /*recycling_bin*/,
             std::vector<Mutation>& /*mutations*/,
