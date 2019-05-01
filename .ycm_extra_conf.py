9264b079997756d919639fae862f00f321ba0946
molpopgen <krthornt@uci.edu>
2019-05-01 14:54:02 -0700

    Add YCM config file

---
+++ .ycm_extra_conf.py
@@ -0,0 +1,21 @@
+import os
+import ycm_core
+
+cwd=os.path.dirname(os.path.realpath(__file__))
+
+FLAGS = [
+        '-Wall',
+        '-Weffc++',
+        '-std=c++14',
+        '-Werr',
+        '-x', 'c++',
+        '-I', '.',
+        '-I', cwd+'/fwdpy11/headers/fwdpp',
+        '-I', cwd+'/fwdpy11/headers'
+        ]
+
+def FlagsForFile( filename ):
+  dirname = os.path.dirname(filename)
+  flags = {'flags': FLAGS,
+      'do_cache': True}
+  return flags
