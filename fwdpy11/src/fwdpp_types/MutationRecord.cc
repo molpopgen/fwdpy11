f4b40c920c92855a50f5b5bbb9facae5561c3645
molpopgen <krthornt@uci.edu>
2019-07-14 12:44:34 -0700

    Add new fields from fwdpp to fwdpy11.MutationRecord and the numpy dtype for the same.

--- fwdpy11/src/fwdpp_types/MutationRecord.cc
+++ fwdpy11/src/fwdpp_types/MutationRecord.cc
@@ -8,7 +8,8 @@ namespace py = pybind11;
 void
 init_ts_MutationRecord(py::module& m)
 {
-    PYBIND11_NUMPY_DTYPE(fwdpp::ts::mutation_record, node, key);
+    PYBIND11_NUMPY_DTYPE(fwdpp::ts::mutation_record, node, key, site,
+                         derived_state, neutral);
     py::class_<fwdpp::ts::mutation_record>(m, "MutationRecord",
                                            R"delim(
             A mutation entry in a tree sequence. A MutationRecord
@@ -21,21 +22,36 @@ init_ts_MutationRecord(py::module& m)
                       "Node id of the mutation")
         .def_readonly("key", &fwdpp::ts::mutation_record::key,
                       "Index of the mutation in the population")
+        .def_readonly("site", &fwdpp::ts::mutation_record::key,
+                      "Index of the mutation's site in the population")
+        .def_readonly("derived_state",
+                      &fwdpp::ts::mutation_record::derived_state,
+                      "The derived state of the mutation")
+        .def_readonly("neutral", &fwdpp::ts::mutation_record::neutral,
+                      "Boolean descriptor of whether or not the mutation "
+                      "affects fitness.")
         .def("__repr__",
              [](const fwdpp::ts::mutation_record& self) {
                  std::ostringstream out;
                  out << "MutationRecord(node=" << self.node
-                     << ", key=" << self.key << ")";
+                     << ", key=" << self.key << ", site=" << self.site
+                     << ", derived_state = " << self.derived_state
+                     << ", neutral = " << self.neutral << ')';
                  return out.str();
              })
         .def(py::pickle(
             [](const fwdpp::ts::mutation_record& m) {
-                return py::make_tuple(m.node, m.key);
+                return py::make_tuple(m.node, m.key, m.site, m.derived_state,
+                                      m.neutral);
             },
             [](py::tuple t) {
                 return fwdpp::ts::mutation_record{
                     t[0].cast<decltype(fwdpp::ts::mutation_record::node)>(),
-                    t[1].cast<decltype(fwdpp::ts::mutation_record::key)>()
+                    t[1].cast<decltype(fwdpp::ts::mutation_record::key)>(),
+                    t[2].cast<decltype(fwdpp::ts::mutation_record::site)>(),
+                    t[3].cast<decltype(
+                        fwdpp::ts::mutation_record::derived_state)>(),
+                    t[4].cast<decltype(fwdpp::ts::mutation_record::neutral)>()
                 };
             }));
 }
