4a241fd8bdda837dfef2b76b547a563ac13dff4e
molpopgen <krthornt@uci.edu>
2019-07-22 12:35:49 -0700

    Add test of reading in old binary file format.

---
+++ tests/test_binary_format_compatibility.py
@@ -0,0 +1,13 @@
+import unittest
+import fwdpy11
+
+
+class TestOldBinaryFileFormats(unittest.TestCase):
+    def test_reading_045(self):
+        pop = fwdpy11.DiploidPopulation.load_from_file("tests/v045.bin")
+        for i in pop.tables.mutations:
+            self.assertEqual(pop.mutations[i.key].pos, pop.tables.sites[i.site].position)
+
+
+if __name__ == "__main__":
+    unittest.main()
