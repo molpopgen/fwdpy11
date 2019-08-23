fc774de0c0da4239ae2292e09d9931056cefdfdc
molpopgen <krthornt@uci.edu>
2019-08-23 11:26:59 -0700

    Update example scripts to use DiploidPopulation.sample_timepoints

--- examples/gss/iterate_variants_in_tree_sequences.py
+++ examples/gss/iterate_variants_in_tree_sequences.py
@@ -10,37 +10,23 @@ import fwdpy11
 with lzma.open(sys.argv[1], 'rb') as f:
     pop = pickle.load(f)
 
-nodes = np.array(pop.tables.nodes, copy=False)
-
-ancient_sample_metadata = np.array(pop.ancient_sample_metadata, copy=False)
-alive_sample_metadata = np.array(pop.diploid_metadata, copy=False)
-metadata = np.hstack((ancient_sample_metadata, alive_sample_metadata))
-
-metadata_nodes = metadata['nodes'].flatten()
-metadata_node_times = nodes['time'][metadata_nodes]
-metadata_record_times = nodes['time'][metadata['nodes'][:, 0]]
-
-
 genetic_trait_values_from_sim = []
 genetic_values_from_ts = []
 idx = 0
-for u in np.unique(metadata_node_times):
-    samples_at_time_u = metadata_nodes[np.where(metadata_node_times == u)]
-    vi = fwdpy11.VariantIterator(
-        pop.tables, samples_at_time_u)
-    sum_esizes = np.zeros(len(samples_at_time_u))
+for n, s, m in pop.sample_timepoints():
+    vi = fwdpy11.VariantIterator(pop.tables, s)
+    sum_esizes = np.zeros(len(s))
     for variant in vi:
         g = variant.genotypes
         r = variant.records[0]
         mutant = np.where(g == 1)[0]
         sum_esizes[mutant] += pop.mutations[r.key].s
-    ind = int(len(samples_at_time_u)/2)
+    ind = int(len(s)/2)
     temp_gvalues = np.zeros(ind)
     temp_gvalues += sum_esizes[0::2]
     temp_gvalues += sum_esizes[1::2]
     genetic_values_from_ts.extend(temp_gvalues.tolist())
-    genetic_trait_values_from_sim.extend(
-        metadata['g'][np.where(metadata_record_times == u)[0]].tolist())
+    genetic_trait_values_from_sim.extend(m['g'].tolist())
 
 # plot the time vs trait value
 xyplot = sns.scatterplot(genetic_trait_values_from_sim, genetic_values_from_ts)
--- examples/gss/plot_genetic_values_from_tree_sequences.py
+++ examples/gss/plot_genetic_values_from_tree_sequences.py
@@ -17,20 +17,14 @@ with lzma.open(sys.argv[1], 'rb') as f:
 
 nodes = np.array(pop.tables.nodes, copy=False)
 
-ancient_sample_metadata = np.array(pop.ancient_sample_metadata, copy=False)
-alive_sample_metadata = np.array(pop.diploid_metadata, copy=False)
-metadata = np.hstack((ancient_sample_metadata, alive_sample_metadata))
-
-metadata_times = nodes['time'][metadata['nodes'][:, 0]]
-
+times = []
 mean_trait_values = []
-for u in np.unique(metadata_times):
-    samples_at_time_u = np.where(metadata_times == u)[0]
-    mean_trait_values.append(
-        metadata['g'][samples_at_time_u].mean())
+for t, n, m in pop.sample_timepoints():
+    times.append(t)
+    mean_trait_values.append(m['g'].mean())
 
 # plot the time vs trait value
-xyplot = sns.lineplot(np.unique(metadata_times), mean_trait_values)
+xyplot = sns.lineplot(times, mean_trait_values)
 xyplot.set_xlabel("Time (generations")
 xyplot.set_ylabel("Mean trait value")
 xyplot.get_figure().savefig("mean_trait_value_from_tree_sequences.png",
