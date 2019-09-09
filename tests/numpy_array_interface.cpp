85186ba8cb5aea0c77a85278b30876d6d0a2af3f
molpopgen <krthornt@uci.edu>
2019-09-09 10:54:26 -0700

    Add tests for the 2D array interface.

--- tests/numpy_array_interface.cpp
+++ tests/numpy_array_interface.cpp
@@ -12,16 +12,41 @@ struct VecBackedArray
     VecBackedArray() : x(std::vector<double>(5, 1.0)) {}
 };
 
+struct VecBacked2DArray
+{
+    std::size_t nrow, ncol;
+    std::vector<double> x;
+    VecBacked2DArray(std::size_t nr, std::size_t nc)
+        : nrow(nr), ncol(nc), x(std::vector<double>(ncol * nrow, 0.0))
+    {
+    }
+};
+
 PYBIND11_MODULE(numpy_array_interface, m)
 {
     py::class_<VecBackedArray>(m, "VecBackedArray")
         .def(py::init<>())
-        .def("x_readwrite", [](const VecBackedArray& self) {
-            return fwdpy11::make_1d_ndarray(self.x);
-        })
+        .def("x_readwrite",
+             [](const VecBackedArray& self) {
+                 return fwdpy11::make_1d_ndarray(self.x);
+             })
         .def("x_readonly", [](const VecBackedArray& self) {
             return fwdpy11::make_1d_ndarray_readonly(self.x);
         });
 
+    py::class_<VecBacked2DArray>(m, "VecBacked2DArray")
+        .def(py::init<std::size_t, std::size_t>())
+        .def("x_readwrite",
+             [](const VecBacked2DArray& self) {
+                 return fwdpy11::make_2d_ndarray(self.x, self.nrow, self.ncol);
+             })
+        .def("x_readonly",
+             [](const VecBacked2DArray& self) {
+                 return fwdpy11::make_2d_ndarray_readonly(self.x, self.nrow,
+                                                          self.ncol);
+             })
+        .def_property_readonly("shape", [](const VecBacked2DArray& self) {
+            return py::make_tuple(self.nrow, self.ncol);
+        });
 }
 
--- tests/test_numpy_array_interface.py
+++ tests/test_numpy_array_interface.py
@@ -2,10 +2,30 @@ import unittest
 import numpy_array_interface
 
 
-class TestNumpyArrayInterface(unittest.TestCase):
+class TestNumpyArrayInterface1D(unittest.TestCase):
     @classmethod
     def setUp(self):
         self.v = numpy_array_interface.VecBackedArray()
+
+    def test_readwrite(self):
+        a = self.v.x_readwrite()
+        self.assertEqual(a.flags.owndata, False)
+        self.assertEqual(a.flags.writeable, True)
+
+    def test_readonly(self):
+        a = self.v.x_readonly()
+        self.assertEqual(a.flags.owndata, False)
+        self.assertEqual(a.flags.writeable, False)
+
+
+class TestNumpyArrayInterface2D(unittest.TestCase):
+    @classmethod
+    def setUp(self):
+        self.v = numpy_array_interface.VecBacked2DArray(7, 3)
+
+    def test_shape(self):
+        a = self.v.x_readwrite()
+        self.assertEqual(a.shape, self.v.shape)
 
     def test_readwrite(self):
         a = self.v.x_readwrite()
