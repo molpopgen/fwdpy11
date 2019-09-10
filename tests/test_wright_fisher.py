fe9e67bba754ccd81144a0a130843fb9e7228327
molpopgen <krthornt@uci.edu>
2019-09-10 12:13:11 -0700

    Remove use of Cython-based modules from tests

--- tests/test_wright_fisher.py
+++ tests/test_wright_fisher.py
@@ -1,9 +1,6 @@
 import unittest
 import fwdpy11 as fp11
-import pyximport
 import numpy as np
-pyximport.install(setup_args={'include_dirs': np.get_include()})  # noqa
-from MeanFitness import MeanFitness
 
 
 class GenerationRecorder(object):
@@ -22,7 +19,6 @@ class testWFevolve(unittest.TestCase):
         self.pop = fp11.DiploidPopulation(1000)
         self.rng = fp11.GSLrng(42)
         self.recorder = GenerationRecorder()
-        self.cython_recorder = MeanFitness()
         self.p = ModelParams()
         self.p.rates = (1e-3, 1e-3, 1e-3)
         self.p.demography = np.array([1000] * 100, dtype=np.uint32)
@@ -40,27 +36,6 @@ class testWFevolve(unittest.TestCase):
         evolve(self.rng, self.pop, self.p, self.recorder)
         self.assertEqual(self.recorder.generations,
                          [i + 1 for i in range(124)])
-
-
-class testCythonRecorder(unittest.TestCase):
-    @classmethod
-    def setUpClass(self):
-        from fwdpy11 import ModelParams
-        from fwdpy11 import Multiplicative
-        self.pop = fp11.DiploidPopulation(1000)
-        self.rng = fp11.GSLrng(42)
-        self.cython_recorder = MeanFitness()
-        self.p = ModelParams()
-        self.p.rates = (1e-3, 1e-3, 1e-3)
-        self.p.demography = np.array([1000] * 100, dtype=np.uint32)
-        self.p.nregions = [fp11.Region(0, 1, 1)]
-        self.p.sregions = [fp11.ExpS(0, 1, 1, -1e-2)]
-        self.p.recregions = self.p.nregions
-        self.p.gvalue = Multiplicative(2.0)
-
-    def testEvolve(self):
-        from fwdpy11 import evolve_genomes as evolve
-        evolve(self.rng, self.pop, self.p, self.cython_recorder)
 
 
 if __name__ == "__main__":
