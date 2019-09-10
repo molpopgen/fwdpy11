6931e61076233f80f8977d237c5da9568af3594d
Kevin R. Thornton <krthornt@uci.edu>
2019-09-10 12:49:38 -0700

    Update travis ci (#303)
    Update Travis CI to test Ubuntu 18.04 where possible.  The conda build config now uses the "latest" version with the GCC 7.x compilers.

--- travis_scripts/installation_script.sh
+++ travis_scripts/installation_script.sh
@@ -6,7 +6,7 @@ then
     echo "HERE"
     if [ "$TRAVIS_OS_NAME" == "linux" ]; 
     then 
-        wget https://repo.continuum.io/miniconda/Miniconda3-4.3.21-Linux-x86_64.sh -O miniconda.sh;
+        wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
     fi
     if [ "$TRAVIS_OS_NAME" == "osx" ];
     then 
@@ -19,15 +19,17 @@ then
     #conda update -q conda
     # Useful for debugging any issues with conda
     conda info -a
+    conda create -q -n test-environment
+    condat activate test-environment
     if [ "$TRAVIS_OS_NAME" == "linux" ];
-    then conda install gcc;
+    then conda install gcc_linux-64 gxx_linux-64;
     fi
     if [ "$TRAVIS_OS_NAME" == "osx" -a "$OSXGCC" == "1" ];
     then
         conda install gcc;
     fi
-    conda install cython numpy python==3.6 gsl
-    conda install -c conda-forge pybind11==2.2.4 numpy==1.16.2 msprime==0.7.0 openblas cmake
+    conda install cython numpy gsl
+    conda install -c conda-forge pybind11==2.3.0 numpy msprime openblas cmake
     # Weird stuff is happening right now, so we force numpy and numpy base:
     # conda install -c conda-forge numpy==1.16.2 numpy-base==1.16.2
     # conda install -c conda-forge sphinx nbsphinx ipython matplotlib msprime
@@ -35,14 +37,18 @@ then
     echo `which python3`
 else
     sudo apt-get update -qq
-    sudo apt-get -f install python-dev libffi-dev libssl-dev libhdf5-dev cmake
-    ./travis_scripts/gsl2.sh
+    if [ "$XENIAL" == "1" ];
+    then
+        sudo apt-get -f install python-dev libffi-dev libssl-dev libhdf5-dev cmake;
+        ./travis_scripts/gsl2.sh;
+    else sudo apt-get -f install python-dev libffi-dev libssl-dev libhdf5-dev cmake libgsl-dev;
+    fi
     pip install --upgrade -r requirements.txt
 
     # We need to get pybind11 NOT from pip
-    curl -L https://github.com/pybind/pybind11/archive/v2.2.4.tar.gz > pybind11-2.2.4.tar.gz
-    tar xzf pybind11-2.2.4.tar.gz
+    curl -L https://github.com/pybind/pybind11/archive/v2.3.0.tar.gz > pybind11-2.3.0.tar.gz
+    tar xzf pybind11-2.3.0.tar.gz
     echo "python version is $TRAVIS_PYTHON_VERSION"
     # Skip running the pybind11 unit suite--not needed...
-    cd pybind11-2.2.4 && cmake . -DPYBIND11_PYTHON_VERSION=$TRAVIS_PYTHON_VERSION -DPYBIND11_TEST=0 && sudo make install && pip install . && cd ..
+    cd pybind11-2.3.0 && cmake . -DPYBIND11_PYTHON_VERSION=$TRAVIS_PYTHON_VERSION -DPYBIND11_TEST=0 && sudo make install && pip install . && cd ..
 fi
