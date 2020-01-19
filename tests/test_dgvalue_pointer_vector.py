d09be9a5300fe1dc4a3efe1585a9c283e883de0a
molpopgen <krthornt@uci.edu>
2020-01-19 12:17:17 -0800

    Add tests of bad input to _dgvalue_pointer_vector

--- tests/test_dgvalue_pointer_vector.py
+++ tests/test_dgvalue_pointer_vector.py
@@ -1,5 +1,6 @@
 import fwdpy11
 import unittest
+import numpy as np
 
 
 class TestConstruction(unittest.TestCase):
@@ -15,6 +16,26 @@ class TestConstruction(unittest.TestCase):
     def test_construct_from_list(self):
         from fwdpy11._fwdpy11 import _dgvalue_pointer_vector
         _dgvalue_pointer_vector(self.list)
+
+
+class TestBadInput(unittest.TestCase):
+    def test_construct_from_empty_list(self):
+        from fwdpy11._fwdpy11 import _dgvalue_pointer_vector
+        with self.assertRaises(ValueError):
+            _dgvalue_pointer_vector([])
+
+    def test_invalid_dimensionality(self):
+        a = fwdpy11.Additive(2.)
+        mvgss = fwdpy11.MultivariateGSS(np.zeros(2), 10)
+        mv = fwdpy11.StrictAdditiveMultivariateEffects(2, 0, mvgss)
+        from fwdpy11._fwdpy11 import _dgvalue_pointer_vector
+        with self.assertRaises(ValueError):
+            _dgvalue_pointer_vector([a, mv])
+
+    def test_invalid_types(self):
+        from fwdpy11._fwdpy11 import _dgvalue_pointer_vector
+        with self.assertRaises(RuntimeError):
+            _dgvalue_pointer_vector([1, 2])
 
 
 if __name__ == "__main__":
