c7d6ee1600b2f7fb444430971396f87ce2d53535
molpopgen <krthornt@uci.edu>
2020-02-15 15:22:43 -0800

    Add fwdpy11.migration_matrix_single_extant_deme

--- fwdpy11/_demography.py
+++ fwdpy11/_demography.py
@@ -14,3 +14,20 @@ def exponential_growth_rate(N0, Nt, time):
         raise ValueError("N0 and Nt must both be >= 1")
     import numpy as np
     return np.exp((np.log(Nt) - np.log(N0))/time)
+
+
+def migration_matrix_single_extant_deme(ndemes, focal_deme):
+    """
+    Sets up a migration matrix for a system of ndemes
+    but where only one deme has a size > 0.  This
+    is a convenience function for simulations where
+    demes will appear later on and have migration amongst them.
+
+    :param ndemes: The number of demes that will eventually exist.
+    :param focal_deme: The index of the initial extant deme.
+    :rtype: numpy.ndarray
+    """
+    import numpy as np
+    m = np.zeros(ndemes*ndemes).reshape(ndemes, ndemes)
+    m[focal_deme, focal_deme] = 1.0
+    return m
