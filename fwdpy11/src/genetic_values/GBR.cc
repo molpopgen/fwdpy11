e97e0e02082ee81626c39b78fd1f3f178e98ea1a
molpopgen <krthornt@uci.edu>
2020-03-14 15:32:35 -0700

    Add new C++ back end to fwdpp's site_dependent_genetic_value. fwdpy11.Additive and fwdpy11.Multiplicative use this new back-end and are entirely self-contained in the .cc files. fwdpy11.GBR is now also self-contained in its .cc file. These changes allow for mutations having different effect sizes in different demes.

--- fwdpy11/src/genetic_values/GBR.cc
+++ fwdpy11/src/genetic_values/GBR.cc
@@ -1,7 +1,109 @@
-#include <fwdpy11/genetic_values/DiploidGBR.hpp>
+#include <fwdpy11/genetic_values/DiploidGeneticValue.hpp>
+#include <fwdpy11/genetic_value_to_fitness/GeneticValueToFitnessMap.hpp>
+#include <fwdpy11/genetic_value_noise/GeneticValueNoise.hpp>
 #include <pybind11/pybind11.h>
 
 namespace py = pybind11;
+
+namespace
+{
+    class GBR : public fwdpy11::DiploidGeneticValue
+    {
+      private:
+        mutable std::size_t deme;
+        template <typename mutation_key_cont_t, typename mcont_t>
+        inline double
+        sum_haplotype_effect_sizes(const mutation_key_cont_t& keys,
+                                   const mcont_t& mutations) const
+        {
+            double rv = 0.0;
+            for (auto& k : keys)
+                {
+                    rv += mutations[k].s;
+                }
+            return rv;
+        }
+
+        template <typename mutation_key_cont_t, typename mcont_t>
+        inline double
+        sum_haplotype_effect_sizes(std::size_t deme, const mutation_key_cont_t& keys,
+                                   const mcont_t& mutations) const
+        {
+            double rv = 0.0;
+            for (auto& k : keys)
+                {
+                    rv += mutations[k].esizes[deme];
+                }
+            return rv;
+        }
+
+        std::function<double(const std::size_t diploid_index,
+                             const fwdpy11::DiploidPopulation& pop)>
+        generate_backend_function(std::size_t ndemes, std::size_t& deme)
+        {
+            if (ndemes == 1)
+                {
+                    return [this](const std::size_t diploid_index,
+                                  const fwdpy11::DiploidPopulation& pop) {
+                        double h1 = sum_haplotype_effect_sizes(
+                            pop.haploid_genomes[pop.diploids[diploid_index].first]
+                                .smutations,
+                            pop.mutations);
+                        double h2 = sum_haplotype_effect_sizes(
+                            pop.haploid_genomes[pop.diploids[diploid_index].second]
+                                .smutations,
+                            pop.mutations);
+                        return sqrt(h1 * h2);
+                    };
+                }
+            return [&deme, this](const std::size_t diploid_index,
+                                 const fwdpy11::DiploidPopulation& pop) {
+                double h1 = sum_haplotype_effect_sizes(
+                    deme,
+                    pop.haploid_genomes[pop.diploids[diploid_index].first].smutations,
+                    pop.mutations);
+                double h2 = sum_haplotype_effect_sizes(
+                    deme,
+                    pop.haploid_genomes[pop.diploids[diploid_index].second].smutations,
+                    pop.mutations);
+                return sqrt(h1 * h2);
+            };
+        }
+
+        const std::function<double(const std::size_t diploid_index,
+                                   const fwdpy11::DiploidPopulation& pop)>
+            f;
+
+      public:
+        GBR(std::size_t ndim, const fwdpy11::GeneticValueToFitnessMap& gv2w_)
+            : fwdpy11::DiploidGeneticValue{ndim, gv2w_},
+              deme(0), f{generate_backend_function(total_dim, deme)}
+        {
+        }
+
+        GBR(std::size_t ndim, const fwdpy11::GeneticValueToFitnessMap& gv2w_,
+            const fwdpy11::GeneticValueNoise& noise_)
+            : fwdpy11::DiploidGeneticValue{ndim, gv2w_, noise_},
+              deme(0), f{generate_backend_function(total_dim, deme)}
+        {
+        }
+
+        double
+        calculate_gvalue(const std::size_t diploid_index,
+                         const fwdpy11::DiploidPopulation& pop) const override
+        {
+            deme = pop.diploid_metadata[diploid_index].deme;
+            gvalues[0] = f(diploid_index, pop);
+            return gvalues[0];
+        }
+
+        pybind11::object
+        pickle() const override
+        {
+            return py::int_(total_dim);
+        }
+    };
+}
 
 static const auto GBR_CONSTRUCTOR1 =
     R"delim(
@@ -25,8 +127,8 @@ and random effects on trait value.
 void
 init_GBR(py::module& m)
 {
-    py::class_<fwdpy11::DiploidGBR, fwdpy11::DiploidGeneticValue>(m, "GBR",
-                                                                  R"delim(
+    py::class_<GBR, fwdpy11::DiploidGeneticValue>(m, "GBR",
+                                                  R"delim(
         The "gene-based recessive" trait model described in Thornton et al.
         2013 http://dx.doi.org/10.1371/journal.pgen.1003258 and Sanjak et al. 2017
         http://dx.doi.org/10.1371/journal.pgen.1006573.
@@ -34,41 +136,33 @@ init_GBR(py::module& m)
         The trait value is the geometric mean of the sum of effect sizes on each haplotype.
         It is undefined for the case where these sums are negative.
         )delim")
-        .def(py::init([](const fwdpy11::GeneticValueIsTrait& gv2w) {
-                 return fwdpy11::DiploidGBR(fwdpy11::GBR{}, gv2w);
-             }),
+        .def(py::init(
+                 [](const fwdpy11::GeneticValueIsTrait& gv2w) { return GBR(1, gv2w); }),
              py::arg("gv2w"), GBR_CONSTRUCTOR1)
         .def(py::init([](const fwdpy11::GeneticValueIsTrait& gv2w,
                          const fwdpy11::GeneticValueNoise& noise) {
-                 return fwdpy11::DiploidGBR(fwdpy11::GBR{}, gv2w, noise);
+                 return GBR(1, gv2w, noise);
              }),
              py::arg("gv2w"), py::arg("noise"), GBR_CONSTRUCTOR2)
         .def(py::pickle(
-            [](const fwdpy11::DiploidGBR& g) {
+            [](const GBR& g) {
                 auto p = py::module::import("pickle");
-                return py::make_tuple(
-                    g.pickle(), p.attr("dumps")(g.gv2w->clone(), -1),
-                    p.attr("dumps")(g.noise_fxn->clone(), -1));
+                return py::make_tuple(g.pickle(), p.attr("dumps")(g.gv2w->clone(), -1),
+                                      p.attr("dumps")(g.noise_fxn->clone(), -1));
             },
             [](py::tuple t) {
                 if (t.size() != 3)
                     {
                         throw std::runtime_error("invalid object state");
                     }
-                std::string s = t[0].cast<std::string>();
-                if (s != "GBR")
-                    {
-                        throw std::runtime_error("invalid object state");
-                    }
+                auto ndim = t[0].cast<std::size_t>();
                 auto p = py::module::import("pickle");
                 auto t1 = p.attr("loads")(t[1]);
                 auto t2 = p.attr("loads")(t[2]);
                 //Do the casts in the constructor
                 //to avoid any nasty issues w/
                 //refs to temp
-                return fwdpy11::DiploidGBR(
-                    fwdpy11::GBR{},
-                    t1.cast<const fwdpy11::GeneticValueIsTrait&>(),
-                    t2.cast<const fwdpy11::GeneticValueNoise&>());
+                return GBR(ndim, t1.cast<const fwdpy11::GeneticValueIsTrait&>(),
+                           t2.cast<const fwdpy11::GeneticValueNoise&>());
             }));
 }
