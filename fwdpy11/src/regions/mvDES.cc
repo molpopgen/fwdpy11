f4f58fd7a3e62c5aca3f62b8d5d83519bfb2a66f
molpopgen <krthornt@uci.edu>
2020-03-16 13:26:16 -0700

    Start adding details to mvDES Python class.

--- fwdpy11/src/regions/mvDES.cc
+++ fwdpy11/src/regions/mvDES.cc
@@ -1,10 +1,58 @@
+#include <memory>
+#include <functional>
+#include <gsl/gsl_matrix.h>
 #include <pybind11/pybind11.h>
+#include <pybind11/numpy.h>
+#include <pybind11/stl.h>
 #include <fwdpy11/regions/mvDES.hpp>
 
 namespace py = pybind11;
 
+namespace
+{
+    using matrix_ptr = std::unique_ptr<gsl_matrix, std::function<void(gsl_matrix *)>>;
+    fwdpy11::mvDES
+    create_from_python(py::array_t<double> means, py::array_t<double> vcov,
+                       py::list sregions)
+    {
+        auto means_buffer = means.request();
+        if (means_buffer.ndim != 1)
+            {
+                throw std ::invalid_argument("means must be a 1d ndarray");
+            }
+        std::vector<double> mu(static_cast<double *>(means_buffer.ptr),
+                               static_cast<double *>(means_buffer.ptr) + means.shape(0));
+        auto vcov_unchecked = vcov.unchecked<2>();
+        if (vcov_unchecked.shape(0) != vcov_unchecked.shape(1))
+            {
+                throw std::invalid_argument("input matrix must be square");
+            }
+        matrix_ptr m(gsl_matrix_alloc(vcov_unchecked.shape(0), vcov_unchecked.shape(1)),
+                     [](gsl_matrix *m) { gsl_matrix_free(m); });
+        for (py::ssize_t i = 0; i < vcov_unchecked.shape(0); ++i)
+            {
+                for (py::ssize_t j = 0; j < vcov_unchecked.shape(1); ++j)
+                    {
+                        gsl_matrix_set(m.get(), i, j, vcov_unchecked(i, j));
+                    }
+            }
+
+        std::vector<std::unique_ptr<fwdpy11::Sregion>> output_distributions;
+        for (auto s : sregions)
+            {
+                output_distributions.emplace_back(
+                    s.cast<const fwdpy11::Sregion &>().clone());
+            }
+        return fwdpy11::mvDES(output_distributions, std::move(mu), *m);
+    }
+}
+
 void
 init_mvDES(py::module &m)
 {
-    py::class_<fwdpy11::mvDES, fwdpy11::Sregion>(m, "mvDES");
+    py::class_<fwdpy11::mvDES, fwdpy11::Sregion>(m, "mvDES")
+        .def(py::init(
+            [](py::array_t<double> means, py::array_t<double> vcov, py::list sregions) {
+                return create_from_python(means, vcov, sregions);
+            }));
 }
