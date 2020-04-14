f7ba7398804b9e56b6caad35682245a2ac092227
molpopgen <krthornt@uci.edu>
2020-04-14 14:49:58 -0700

    Add __repr__ to GeneticMapUnit classes.

--- fwdpy11/src/fwdpp_types/BinomialPoint.cc
+++ fwdpy11/src/fwdpp_types/BinomialPoint.cc
@@ -2,12 +2,12 @@
 #include <fwdpp/genetic_map/binomial_point.hpp>
 
 namespace py = pybind11;
+using namespace py::literals;
 
 void
 init_BinomialPoint(py::module& m)
 {
-    py::class_<fwdpp::binomial_point, fwdpp::genetic_map_unit>(m,
-                                                               "BinomialPoint",
+    py::class_<fwdpp::binomial_point, fwdpp::genetic_map_unit>(m, "BinomialPoint",
                                                                R"delim(
         Generate a crossover breakpoint at a fixed position with a
         fixed probability.  This class represents genetic distance
@@ -18,20 +18,26 @@ init_BinomialPoint(py::module& m)
         .. versionchanged:: 0.5.0
 
             Refactored back-end to be based on fwdpp types
+
+        .. versionchanged:: 0.7.1
+
+            Add __repr__
         )delim")
-        .def(py::init<double, double>(), py::arg("position"),
-             py::arg("probability"))
+        .def(py::init<double, double>(), py::arg("position"), py::arg("probability"))
         .def_readonly("position", &fwdpp::binomial_point::position, "Position")
-        .def_readonly("probability", &fwdpp::binomial_point::prob,
-                      "Probability")
+        .def_readonly("probability", &fwdpp::binomial_point::prob, "Probability")
+        .def("__repr__",
+             [](const fwdpp::binomial_point& self) {
+                 return "BinomialPoint(position={}, probability={})"_s.format(
+                     self.position, self.prob);
+             })
         .def(py::pickle(
             [](const fwdpp::binomial_point& self) {
                 return py::make_tuple(self.position, self.prob);
             },
             [](py::tuple t) {
                 return std::unique_ptr<fwdpp::binomial_point>(
-                    new fwdpp::binomial_point(t[0].cast<double>(),
-                                              t[1].cast<double>()));
+                    new fwdpp::binomial_point(t[0].cast<double>(), t[1].cast<double>()));
             }));
 }
 
