353220f33c6cb7f5c2746c9418c1d5eb462fc70d
molpopgen <krthornt@uci.edu>
2020-04-14 13:45:06 -0700

    Refactor docstrings of several Sregion classes. Closes #450. Also see #329.

--- fwdpy11/src/regions/ConstantS.cc
+++ fwdpy11/src/regions/ConstantS.cc
@@ -3,54 +3,59 @@
 
 namespace py = pybind11;
 
+static const auto INIT_DOCSTRING =
+    R"delim(
+Constructor
+
+:param beg: the beginning of the region
+:type beg: float
+:param end: the end of the region
+:type end: float
+:param weight: the weight to assign
+:type weight: float
+:param s: the selection coefficient
+:type s: float
+:param h: the dominance
+:type h: float
+:param coupled: if True, the weight is converted to (end-beg)*weight
+:type coupled: bool
+:param label: Fill :attr:`fwdpy11.Mutation.label` with this value.
+:type label: numpy.uint16
+:param scaling: The scaling of the DFE
+:type scaling: float
+
+When coupled is True, the "weight" may be interpreted
+as a "per base pair"
+(or per unit, generally speaking) term.
+
+Example:
+
+.. testcode::
+
+    # A simple case
+    import fwdpy11
+    # s = -0.1 and h = 0
+    constantS = fwdpy11.ConstantS(0,1,1,-0.1,0)
+)delim";
+
 void
 init_ConstantS(py::module& m)
 {
-    py::class_<fwdpy11::ConstantS, fwdpy11::Sregion>(
-        m, "ConstantS", "Mutations with fixed effect sizes")
-        .def(py::init([](double beg, double end, double weight, double s,
-                         double h, bool coupled, std::uint16_t label,
-                         double scaling) {
+    py::class_<fwdpy11::ConstantS, fwdpy11::Sregion>(m, "ConstantS",
+                                                     "Mutations with fixed effect sizes")
+        .def(py::init([](double beg, double end, double weight, double s, double h,
+                         bool coupled, std::uint16_t label, double scaling) {
                  return fwdpy11::ConstantS(
-                     fwdpy11::Region(beg, end, weight, coupled, label),
-                     scaling, s, h);
+                     fwdpy11::Region(beg, end, weight, coupled, label), scaling, s, h);
              }),
              py::arg("beg"), py::arg("end"), py::arg("weight"), py::arg("s"),
-             py::arg("h") = 1.0, py::arg("coupled") = true,
-             py::arg("label") = 0, py::arg("scaling") = 1.0,
-             R"delim(
-            Constructor
-
-            :param beg: the beginning of the region
-            :param end: the end of the region
-            :param weight: the weight to assign
-            :param s: the selection coefficient
-            :param h: the dominance
-            :param coupled: if True, the weight is converted to (end-beg)*weight
-            :param label: Not relevant to recombining regions.
-                Otherwise, this value will be used to take mutations
-                from this region.
-            :param scaling: The scaling of the DFE
-
-            When coupled is True, the "weight" may be interpreted
-            as a "per base pair"
-            (or per unit, generally speaking) term.
-
-            Example:
-
-            .. testcode::
-
-                #A simple case
-                import fwdpy11
-                #s = -0.1 and h = 0
-                constantS = fwdpy11.ConstantS(0,1,1,-0.1,0)
-            )delim")
+             py::arg("h") = 1.0, py::arg("coupled") = true, py::arg("label") = 0,
+             py::arg("scaling") = 1.0, INIT_DOCSTRING)
         .def_readonly("esize", &fwdpy11::ConstantS::esize)
         .def_readonly("s", &fwdpy11::ConstantS::esize)
         .def_readonly("h", &fwdpy11::ConstantS::dominance)
-        .def("__repr__",&fwdpy11::ConstantS::repr)
-        .def(py::pickle(
-            [](const fwdpy11::ConstantS& self) { return self.pickle(); },
-            [](py::tuple t) { return fwdpy11::ConstantS::unpickle(t); }));
+        .def("__repr__", &fwdpy11::ConstantS::repr)
+        .def(py::pickle([](const fwdpy11::ConstantS& self) { return self.pickle(); },
+                        [](py::tuple t) { return fwdpy11::ConstantS::unpickle(t); }));
 }
 
--- fwdpy11/src/regions/LogNormalS.cc
+++ fwdpy11/src/regions/LogNormalS.cc
@@ -3,10 +3,46 @@
 
 namespace py = pybind11;
 
-static const auto INIT = R"delim(
+static const auto CLASS_DOCSTRING = R"delim(
 Log-normal distribution of effect sizes.
 
 .. versionadded:: 0.7.0
+)delim";
+
+static const auto INIT_DOCSTRING =
+    R"delim(
+Constructor
+
+:param beg: the beginning of the region
+:type beg: float
+:param end: the end of the region
+:type end: float
+:param weight: the weight to assign
+:type weight: float
+:param zeta: the zeta parameter
+:type zeta: float
+:param sigma: the sigma parameter
+:type sigma: float
+:param h: the dominance
+:type h: float
+:param coupled: if True, the weight is converted to(end-beg)*weight
+:type Constructor: bool
+:param label: Fill :attr:`fwdpy11.Mutation.label` with this value.
+:type label: numpy.uint16
+:param scaling: The scaling of the DFE
+:type scaling: float
+
+When coupled is True, the "weight" may be
+interpreted as a "per base pair"
+(or per unit, generally speaking) term.
+
+Example:
+
+.. testcode::
+
+    # A simple case
+    import fwdpy11
+    gdist=fwdpy11.LogNormalS(0, 1, 1, -0.1, 0.35)
 )delim";
 
 static const auto MV_DOCSTRING = R"delim(
@@ -15,21 +51,25 @@ Create an instance compatible with
 details.
 
 :param beg: the beginning of the region
+:type beg: float
 :param end: the end of the region
+:type end: float
 :param weight: the weight to assign
-:param zeta: the zeta parameter
-:param sigma: the sigma parameter
+:type weight: float
 :param h: the dominance
+:type h: float
 :param coupled: if True, the weight is converted to(end-beg)*weight
-:param label: Not relevant to recombining regions.
-    Otherwise, this value will be used
-    to take mutations from this region.
+:type coupled: bool
+:param label: Fill :attr:`fwdpy11.Mutation.label` with this value.
+:type label: numpy.uint16
+:param scaling: The scaling of the DFE
+:type scaling: float
 )delim";
 
 void
 init_LogNormalS(py::module& m)
 {
-    py::class_<fwdpy11::LogNormalS, fwdpy11::Sregion>(m, "LogNormalS", INIT)
+    py::class_<fwdpy11::LogNormalS, fwdpy11::Sregion>(m, "LogNormalS", CLASS_DOCSTRING)
         .def(
             py::init([](double beg, double end, double weight, double zeta, double sigma,
                         double h, bool coupled, std::uint16_t label, double scaling) {
@@ -39,34 +79,7 @@ init_LogNormalS(py::module& m)
             }),
             py::arg("beg"), py::arg("end"), py::arg("weight"), py::arg("zeta"),
             py::arg("sigma"), py::arg("h") = 1.0, py::arg("coupled") = true,
-            py::arg("label") = 0, py::arg("scaling") = 1.0,
-            R"delim(
-            Constructor
-
-            :param beg: the beginning of the region
-            :param end: the end of the region
-            :param weight: the weight to assign
-            :param zeta: the zeta parameter
-            :param sigma: the sigma parameter
-            :param h: the dominance
-            :param coupled: if True, the weight is converted to(end-beg)*weight
-            :param label: Not relevant to recombining regions.
-                Otherwise, this value will be used
-                to take mutations from this region.
-            :param scaling: The scaling of the DFE
-
-            When coupled is True, the "weight" may be
-            interpreted as a "per base pair"
-            (or per unit, generally speaking) term.
-
-            Example:
-
-            .. testcode::
-
-                # A simple case
-                import fwdpy11
-                gdist=fwdpy11.LogNormalS(0, 1, 1, -0.1, 0.35)
-        )delim")
+            py::arg("label") = 0, py::arg("scaling") = 1.0, INIT_DOCSTRING)
         .def_static(
             "mv",
             [](double beg, double end, double weight, double h, bool coupled,
