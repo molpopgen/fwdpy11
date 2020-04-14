46ddc57e62c2783fe71f6969bf8c4fe3f6ee6bec
molpopgen <krthornt@uci.edu>
2020-04-14 14:49:58 -0700

    Add __repr__ to GeneticMapUnit classes.

--- fwdpy11/src/fwdpp_types/FixedCrossovers.cc
+++ fwdpy11/src/fwdpp_types/FixedCrossovers.cc
@@ -2,6 +2,7 @@
 #include <fwdpp/genetic_map/fixed_number_crossovers.hpp>
 
 namespace py = pybind11;
+using namespace py::literals;
 
 void
 init_FixedCrossovers(py::module& m)
@@ -16,24 +17,31 @@ init_FixedCrossovers(py::module& m)
         .. versionchanged:: 0.5.0
 
             Refactored back-end to be based on fwdpp types
+
+        .. versionchanged:: 0.7.1
+
+            Add __repr__
         )delim")
         .def(py::init<double, double, int>(), py::arg("beg"), py::arg("end"),
              py::arg("num_xovers"))
         .def_readonly("beg", &fwdpp::fixed_number_crossovers::beg,
                       "Beginning of interval")
-        .def_readonly("end", &fwdpp::fixed_number_crossovers::end,
-                      "End of interval.")
+        .def_readonly("end", &fwdpp::fixed_number_crossovers::end, "End of interval.")
         .def_readonly("nxovers", &fwdpp::fixed_number_crossovers::nxovers,
                       "Mean number of breakpoints")
+        .def("__repr__",
+             [](const fwdpp::fixed_number_crossovers& self) {
+                 return "FixedCrossovers(beg={}, end={}, nxovers={})"_s.format(
+                     self.beg, self.end, self.nxovers);
+             })
         .def(py::pickle(
             [](const fwdpp::fixed_number_crossovers& self) {
                 return py::make_tuple(self.beg, self.end, self.nxovers);
             },
             [](py::tuple t) {
                 return std::unique_ptr<fwdpp::fixed_number_crossovers>(
-                    new fwdpp::fixed_number_crossovers(t[0].cast<double>(),
-                                                       t[1].cast<double>(),
-                                                       t[2].cast<int>()));
+                    new fwdpp::fixed_number_crossovers(
+                        t[0].cast<double>(), t[1].cast<double>(), t[2].cast<int>()));
             }));
 }
 
