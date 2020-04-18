9cec49406f2e8391945dbedbd283dc68da265dd7
molpopgen <krthornt@uci.edu>
2020-04-18 14:54:16 -0700

    Refactor examples to facilitate profiling in iPython
    rename example scripts
    
    update manual and CI
    
    update section on precapitation vs recapitation performance.

--- examples/tskit/recapitate.py
+++ examples/tskit/recapitate.py
@@ -50,15 +50,25 @@ def make_parser():
     return parser
 
 
-def run_msprime(Ne, rho, theta, model, seed):
-    ts = msprime.simulate(
-        2 * Ne,
-        Ne=Ne,
-        model=model,
-        random_seed=seed,
-        recombination_rate=rho / Ne / 4,
-        mutation_rate=theta / Ne / 4,
-    )
+def run_msprime(Ne, rho, theta, model, seed, input_ts=None):
+    if input_ts is None:
+        ts = msprime.simulate(
+            2 * Ne,
+            Ne=Ne,
+            model=model,
+            random_seed=seed,
+            recombination_rate=rho / Ne / 4,
+            mutation_rate=theta / Ne / 4,
+        )
+    else:
+        ts = msprime.simulate(
+            from_ts=input_ts,
+            Ne=Ne,
+            model=model,
+            random_seed=seed,
+            recombination_rate=rho / Ne / 4,
+        )
+
     return ts
 
 
@@ -73,54 +83,36 @@ def run_msprime_get_stats(Ne, nsam, rho, theta, model, seed):
     return SimOutput(S1, pi1, S2, pi2)
 
 
-def pi_from_fs(fs):
-    nsam = len(fs) - 2
-    i = np.arange(nsam) + 1
-    pi = fs[1:-1] * 2 * (i / nsam) * ((nsam - i) / (nsam - 1))
-    return pi.sum()
-
-
-def process_sim(pop, sample, check_fixations):
-    """
-    Get number of segregating sites and
-    heterozyosity from the frequency spectrum
-    """
-    fs = pop.tables.fs([sample])
-    if check_fixations:
-        assert fs.data[-1] == 0, "hmmm...shouldn't be any fixations!"
-    S = fs.sum()
-    pi = pi_from_fs(fs)
-    return S, pi
-
-
-def run_sim(args):
-    user_args, msprime_seed, fwdpy11_seed = args
+def run_sim(N, rho, theta, simlen, model, nsam, fwdpy11_seed, msprime_seed):
     np.random.seed(msprime_seed)
-    ts = run_msprime(user_args.N, user_args.rho, 0.0, user_args.model, msprime_seed)
-    pop = fwdpy11.DiploidPopulation.create_from_tskit(ts)
-    del ts  # No longer needed
+    pop = fwdpy11.DiploidPopulation(N, 1.0)
 
     pdict = {
         "nregions": [],
         "sregions": [],
         "recregions": [
-            fwdpy11.PoissonInterval(
-                0, pop.tables.genome_length, user_args.rho / pop.N / 4
-            )
+            fwdpy11.PoissonInterval(0, pop.tables.genome_length, rho / pop.N / 4)
         ],
         "rates": (0, 0, None),
         "gvalue": fwdpy11.Multiplicative(1),
         "demography": fwdpy11.DiscreteDemography(),
-        "simlen": user_args.simlen,
+        "simlen": simlen,
     }
     params = fwdpy11.ModelParams(**pdict)
     rng = fwdpy11.GSLrng(fwdpy11_seed)
-    fwdpy11.evolvets(rng, pop, params, 100)
-    fwdpy11.infinite_sites(rng, pop, user_args.theta / pop.N / 4)
-    an = pop.alive_nodes
-    S2N, pi2N = process_sim(pop, an, check_fixations=True)
-    rn = np.random.choice(an, size=user_args.nsam, replace=False)
-    Sn, pin = process_sim(pop, rn, check_fixations=False)
+    fwdpy11.evolvets(rng, pop, params, 100, preserve_first_generation=True)
+    ts = pop.dump_tables_to_tskit()
+    completed_ts = run_msprime(N, rho, 0.0, model, msprime_seed, ts)
+    samples = pop.alive_nodes
+    completed_ts = msprime.mutate(
+        completed_ts, rate=theta / N / 4, random_seed=msprime_seed
+    )
+    S2N = completed_ts.segregating_sites([samples])[0]
+    pi2N = completed_ts.diversity([samples])[0]
+    rsample = np.random.choice(samples, nsam, replace=False)
+    Sn = completed_ts.segregating_sites([rsample])[0]
+    pin = completed_ts.diversity([rsample])[0]
+
     return SimOutput(S2N, pi2N, Sn, pin)
 
 
@@ -146,7 +138,17 @@ if __name__ == "__main__":
     results = []
     with concurrent.futures.ProcessPoolExecutor() as executor:
         futures = {
-            executor.submit(run_sim, (args, i, j))
+            executor.submit(
+                run_sim,
+                args.N,
+                args.rho,
+                args.theta,
+                args.simlen,
+                args.model,
+                args.nsam,
+                i,
+                j,
+            )
             for i, j in zip(msprime_seeds, fwdpy11_seeds)
         }
         for future in concurrent.futures.as_completed(futures):
