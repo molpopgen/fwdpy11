c020c661a65a92b03b81b1f58107230711870da3
molpopgen <krthornt@uci.edu>
2020-04-20 10:39:07 -0700

    Add Tennessen model and new demographic models module

--- doc/pages/demographic_models.rst
+++ doc/pages/demographic_models.rst
@@ -11,3 +11,12 @@ Pre-computed demographic models
 .. automodule:: fwdpy11.demographic_models.IM
    :members:
 
+Human demographic models
+----------------------------------------------------------------------
+
+This module provides the following models:
+
+* [Tennessen2012]_ via :func:`fwdpy11.demographic_models.human.tennessen`.
+
+.. automodule:: fwdpy11.demographic_models.human
+   :members:
---
+++ fwdpy11/demographic_models/human.py
@@ -0,0 +1,124 @@
+"""
+This module provided pre-calculated demographic models
+for human populations.
+"""
+import numpy as np
+
+import fwdpy11
+
+
+def tennessen(burnin: int = 20):
+    """
+    Generate parameters for the demographic model described in:
+
+    Tennessen, Jacob A., Abigail W. Bigham, Timothy D. O’Connor, Wenqing Fu,
+    Eimear E. Kenny, Simon Gravel, Sean McGee, et al. 2012.
+    “Evolution and Functional Impact of Rare Coding Variation
+    from Deep Sequencing of Human Exomes.” Science 337 (6090): 64–69.
+
+    :param burnin: Burn-in time, as a multiplier of the ancestral population size.
+                   Defaults to 20.
+    :type param: int
+
+    :returns: The demographic model, simulation length, and ancestral population size
+    :rtype: tuple
+
+    When this model is run, deme ``0`` corresponds to ``African`` and deme
+    ``1`` corresponds to Eurasian.
+
+    .. note::
+
+        This implementation is based on code provided by Aaron Ragsdale.
+
+    .. versionadded:: 0.7.2
+
+    """
+    Nref = 7310  # Ancestral population dize
+    NAfr0 = 14474  # Initial size change
+    NB = 1861  # Eurasian bottleneck size
+    NAfr = 424000  # Final African pop'n size
+    NEur0 = 1032  # Eurasian second bottleneck size
+    NEur1 = 9237  # Eurasian size as rapid growth starts
+    NEur = 512000  # Final Eurasian pop'n size
+    T_Af = 148000
+    T_B = 51000
+    T_Eu_As = 23000
+    T_accel = 5115
+    mB = 15e-5
+    mF = 2.5e-5
+    generation_time = 25
+
+    # List of demographic events:
+    # keep track of size change, copying, and migration rate change events in
+    # separate lists
+    size_change = []
+    copy = []
+    mig_rates = []
+    growth_rates = []
+
+    # number of generations in epochs
+    T0 = np.rint((T_Af - T_B) / generation_time).astype(int)  # pre-split
+    # split to bottleneck, no growth
+    T1 = np.rint((T_B - T_Eu_As) / generation_time).astype(int)
+    T2 = np.rint((T_Eu_As - T_accel) / generation_time).astype(
+        int
+    )  # Eu growth with r_Eu0
+    # accelerated growth in Af and Eu
+    T3 = np.rint(T_accel / generation_time).astype(int)
+
+    M_init = np.zeros(4).reshape(2, 2)
+    M_init[0, 0] = 1
+    mm = fwdpy11.MigrationMatrix(M_init)
+
+    # burn in for 20*Ne generations
+    gens_burn_in = burnin * Nref
+    total_sim_length = gens_burn_in + T0 + T1 + T2 + T3
+
+    # init: size change of common ancestral population
+    size_change.append(fwdpy11.SetDemeSize(when=gens_burn_in, deme=0, new_size=NAfr0))
+
+    # T0: mass migration, copy from A to Eu bottleneck population
+    copy.append(
+        fwdpy11.copy_individuals(
+            when=gens_burn_in + T0, source=0, destination=1, fraction=NB / NAfr0,
+        )
+    )
+    size_change.append(fwdpy11.SetDemeSize(when=gens_burn_in + T0, deme=1, new_size=NB))
+    # at the same time, set migration rate between deme 0 and 1 to m_A_B
+    mig_rates.append(fwdpy11.SetMigrationRates(gens_burn_in + T0, 0, [1 - mB, mB]))
+    mig_rates.append(fwdpy11.SetMigrationRates(gens_burn_in + T0, 1, [mB, 1 - mB]))
+
+    # T1: adjust size of Eu to Eu0 and set growth rate
+    size_change.append(
+        fwdpy11.SetDemeSize(when=gens_burn_in + T0 + T1, deme=1, new_size=NEur0)
+    )
+    r_Eur0 = (NEur1 / NEur0) ** (1 / T2) - 1
+    growth_rates.append(
+        fwdpy11.SetExponentialGrowth(when=gens_burn_in + T0 + T1, deme=1, G=1 + r_Eur0)
+    )
+    # set migration rates to contemporary rates
+    mig_rates.append(fwdpy11.SetMigrationRates(gens_burn_in + T0 + T1, 0, [1 - mF, mF]))
+    mig_rates.append(fwdpy11.SetMigrationRates(gens_burn_in + T0 + T1, 1, [mF, 1 - mF]))
+
+    # T2: set growth rates to accelerated rates in both populations
+    r_AfrF = (NAfr / NAfr0) ** (1 / T3) - 1
+    r_EurF = (NEur / NEur1) ** (1 / T3) - 1
+    growth_rates.append(
+        fwdpy11.SetExponentialGrowth(
+            when=gens_burn_in + T0 + T1 + T2, deme=0, G=1 + r_AfrF
+        )
+    )
+    growth_rates.append(
+        fwdpy11.SetExponentialGrowth(
+            when=gens_burn_in + T0 + T1 + T2, deme=1, G=1 + r_EurF
+        )
+    )
+
+    ddemog = fwdpy11.DiscreteDemography(
+        mass_migrations=copy,
+        set_deme_sizes=size_change,
+        migmatrix=mm,
+        set_migration_rates=mig_rates,
+        set_growth_rates=growth_rates,
+    )
+    return ddemog, total_sim_length, Nref
