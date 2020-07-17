100e909b1ac933957ca97ba45abb370454413db7
molpopgen <krthornt@uci.edu>
2020-07-17 13:13:14 -0700

    Add option to populate list of mutations in HaploidGenomeProxy.

--- fwdpy11/src/genetic_values/PyDiploidGeneticValue.cc
+++ fwdpy11/src/genetic_values/PyDiploidGeneticValue.cc
@@ -47,14 +47,17 @@ struct genome_data_proxy
     mutation_key_array_proxy smutations_proxy;
     py::object effect_sizes, dominance, smutations, positions;
     std::vector<double> effect_sizes_cpp, dominance_cpp, positions_cpp;
+    py::list pymutations;
+    bool filling_mutations;
 
-    genome_data_proxy()
+    genome_data_proxy(bool fill_mutations_list)
         : effect_sizes_proxy{}, dominance_proxy{}, positions_proxy{}, smutations_proxy{},
           effect_sizes{py::cast<double_array_proxy*>(&effect_sizes_proxy)},
           dominance{py::cast<double_array_proxy*>(&dominance_proxy)},
           smutations{py::cast<mutation_key_array_proxy*>(&smutations_proxy)},
           positions{py::cast<double_array_proxy*>(&positions_proxy)}, effect_sizes_cpp{},
-          dominance_cpp{}, positions_cpp{}
+          dominance_cpp{}, positions_cpp{}, pymutations{}, filling_mutations{
+                                                               fill_mutations_list}
     {
     }
 
@@ -65,12 +68,21 @@ struct genome_data_proxy
         effect_sizes_cpp.clear();
         dominance_cpp.clear();
         positions_cpp.clear();
+        if (filling_mutations)
+            {
+                pymutations.attr("clear")();
+            }
         for (auto k : smutations)
             {
                 auto& m = mutations[k];
                 effect_sizes_cpp.push_back(m.s);
                 dominance_cpp.push_back(m.h);
                 positions_cpp.push_back(m.pos);
+                if (filling_mutations)
+                    {
+                        pymutations.append(
+                            py::cast<const fwdpy11::Mutation*>(&mutations[k]));
+                    }
             }
         effect_sizes_proxy.set(effect_sizes_cpp);
         dominance_proxy.set(dominance_cpp);
@@ -97,8 +109,8 @@ struct PyDiploidGeneticValueData
     py::object offspring_metadata, parent1_metadata, parent2_metadata, genome1, genome2,
         gvalues;
     py::list genomes, parental_metadata;
-    PyDiploidGeneticValueData()
-        : metadata_proxy{}, genome1_data{}, genome2_data{},
+    PyDiploidGeneticValueData(bool fill_mutations_list)
+        : metadata_proxy{}, genome1_data{fill_mutations_list}, genome2_data{fill_mutations_list},
           offspring_metadata{py::cast<fwdpy11::DiploidMetadata*>(&metadata_proxy)},
           parent1_metadata{py::cast<fwdpy11::DiploidMetadata*>(&parent1_metadata_proxy)},
           parent2_metadata{py::cast<fwdpy11::DiploidMetadata*>(&parent2_metadata_proxy)},
@@ -137,10 +149,11 @@ class PyDiploidGeneticValue : public fwdpy11::DiploidGeneticValue
     }
 
   public:
+    bool filling_mutations;
     PyDiploidGeneticValue(std::size_t ndim, py::object gvalue_to_fitness_map,
-                          py::object noise)
+                          py::object noise, bool fill_mutations_list)
         : fwdpy11::DiploidGeneticValue(ndim, *dispatch_gv2w(ndim, gvalue_to_fitness_map),
-                                       *dispatch_noise(noise))
+                                       *dispatch_noise(noise)), filling_mutations{fill_mutations_list}
     {
     }
 };
@@ -153,8 +166,8 @@ class PyDiploidGeneticValueTrampoline : public PyDiploidGeneticValue
 
   public:
     PyDiploidGeneticValueTrampoline(std::size_t ndim, py::object gvalue_to_fitness_map,
-                                    py::object noise)
-        : PyDiploidGeneticValue(ndim, gvalue_to_fitness_map, noise), data{},
+                                    py::object noise, bool fill_mutations_list)
+        : PyDiploidGeneticValue(ndim, gvalue_to_fitness_map, noise, fill_mutations_list), data{fill_mutations_list},
           pydata{py::cast<PyDiploidGeneticValueData*>(&data)}
     {
     }
@@ -200,7 +213,7 @@ init_PyDiploidGeneticValue(py::module& m)
 {
     py::class_<PyDiploidGeneticValue, fwdpy11::DiploidGeneticValue,
                PyDiploidGeneticValueTrampoline>(m, "PyDiploidGeneticValue")
-        .def(py::init<std::size_t, py::object, py::object>())
+        .def(py::init<std::size_t, py::object, py::object, bool>())
         .def("update_members",
              [](PyDiploidGeneticValue& self, const fwdpy11::DiploidPopulation& pop) {
                  self.gv2w->update(pop);
@@ -210,8 +223,7 @@ init_PyDiploidGeneticValue(py::module& m)
     py::class_<PyDiploidGeneticValueData>(m, "PyDiploidGeneticValueData")
         .def_readwrite("offspring_metadata",
                        &PyDiploidGeneticValueData::offspring_metadata)
-        .def_readwrite("parent1_metadata",
-                       &PyDiploidGeneticValueData::parent1_metadata)
+        .def_readwrite("parent1_metadata", &PyDiploidGeneticValueData::parent1_metadata)
         .def_readwrite("parental_metadata",
                        &PyDiploidGeneticValueData::parental_metadata)
         .def_readwrite("gvalues", &PyDiploidGeneticValueData::gvalues)
@@ -221,7 +233,8 @@ init_PyDiploidGeneticValue(py::module& m)
         .def_readonly("effect_sizes", &genome_data_proxy::effect_sizes)
         .def_readonly("dominance", &genome_data_proxy::dominance)
         .def_readonly("positions", &genome_data_proxy::positions)
-        .def_readonly("smutations", &genome_data_proxy::smutations);
+        .def_readonly("smutations", &genome_data_proxy::smutations)
+        .def_readonly("mutations", &genome_data_proxy::pymutations);
 
     py::class_<double_array_proxy>(m, "_FloatProxy", py::buffer_protocol())
         .def_buffer([](const double_array_proxy& self) {
