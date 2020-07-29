ee0c55033dcfabe31a21b3282d6dafa0e9d1794a
molpopgen <krthornt@uci.edu>
2020-07-29 10:35:01 -0700

    Refactor initialization of DiploidGeneticValue to take const pointers as input and set defaults if nullptr.

--- fwdpy11/src/genetic_values/DiploidMultivariateEffectsStrictAdditive.cc
+++ fwdpy11/src/genetic_values/DiploidMultivariateEffectsStrictAdditive.cc
@@ -9,16 +9,10 @@ init_DiploidMultivariateEffectsStrictAdditive(py::module& m)
     py::class_<fwdpy11::DiploidMultivariateEffectsStrictAdditive,
                fwdpy11::DiploidGeneticValue>(m, "_ll_StrictAdditiveMultivariateEffects")
         .def(py::init([](std::size_t ndimensions, std::size_t focal_trait,
-                         const fwdpy11::GeneticValueIsTrait& gvalue_to_fitness,
-                         py::object noise) {
-                 if (noise.is_none())
-                     {
-                         return fwdpy11::DiploidMultivariateEffectsStrictAdditive(
-                             ndimensions, focal_trait, gvalue_to_fitness);
-                     }
+                         const fwdpy11::GeneticValueIsTrait* gvalue_to_fitness,
+                         const fwdpy11::GeneticValueNoise* noise) {
                  return fwdpy11::DiploidMultivariateEffectsStrictAdditive(
-                     ndimensions, focal_trait, gvalue_to_fitness,
-                     noise.cast<const fwdpy11::GeneticValueNoise&>());
+                     ndimensions, focal_trait, gvalue_to_fitness, noise);
              }),
              py::arg("ndimensions"), py::arg("focal_trait"),
              py::arg("gvalue_to_fitness"), py::arg("noise"));
--- tests/ll_snowdrift.cc
+++ tests/ll_snowdrift.cc
@@ -39,10 +39,9 @@ struct snowdrift : public fwdpy11::DiploidGeneticValue
     const fwdpp::additive_diploid additive;
 
     // This constructor is exposed to Python
-    snowdrift(double b1_, double b2_, double c1_, double c2_,
-              double slope, double p0)
-        : fwdpy11::DiploidGeneticValue{1}, b1(b1_), b2(b2_), c1(c1_), c2(c2_),
-          slope(slope), sig0(1. / slope * std::log(p0 / (1. - p0))),
+    snowdrift(double b1_, double b2_, double c1_, double c2_, double slope, double p0)
+        : fwdpy11::DiploidGeneticValue{1, nullptr, nullptr}, b1(b1_), b2(b2_), c1(c1_),
+          c2(c2_), slope(slope), sig0(1. / slope * std::log(p0 / (1. - p0))),
           phenotypes(), additive{fwdpp::trait{2.0}}
     {
         if (p0 == 0 || p0 == 1.)
@@ -103,8 +102,8 @@ PYBIND11_MODULE(ll_snowdrift, m)
 
     // Create a Python class based on our new type
     py::class_<snowdrift, fwdpy11::DiploidGeneticValue>(m, "_ll_DiploidSnowdrift")
-        .def(py::init<double, double, double, double, double, double>(),
-             py::arg("b1"), py::arg("b2"), py::arg("c1"), py::arg("c2"),
-             py::arg("slope"), py::arg("p0"))
+        .def(py::init<double, double, double, double, double, double>(), py::arg("b1"),
+             py::arg("b2"), py::arg("c1"), py::arg("c2"), py::arg("slope"),
+             py::arg("p0"))
         .def_readwrite("phenotypes", &snowdrift::phenotypes);
 }
