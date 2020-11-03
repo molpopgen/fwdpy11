827c83e224c7029c300fe2312d6697d3b38e5b12
molpopgen <krthornt@uci.edu>
2020-11-03 11:27:27 -0800

    Remove Travis CI scripts

--- travis_scripts/gsl2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-wget http://mirror.us-midwest-1.nexcess.net/gnu/gsl/gsl-2.3.tar.gz
-tar xzf gsl-2.3.tar.gz
-cd gsl-2.3
-./configure
-make -j 2 > /dev/null 
-sudo make install > /dev/null 2> /dev/null
-cd ..
