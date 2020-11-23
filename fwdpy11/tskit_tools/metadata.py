723c0a1f69c0ab270b7b3845e1f8957c18a8ed5e
molpopgen <krthornt@uci.edu>
2020-11-23 12:38:47 -0800

    Add documentation.

--- fwdpy11/tskit_tools/metadata.py
+++ fwdpy11/tskit_tools/metadata.py
@@ -24,20 +24,71 @@ import tskit
 import fwdpy11
 
 
-@attr.s(auto_attribs=True, kw_only=True, eq=True)
+@attr.s(auto_attribs=True, frozen=True, kw_only=True, eq=True)
 class DiploidMetadata(object):
+    """
+    Instances of this type are returned by
+    :func:`fwdpy11.tskit_tools.decode_individual_metadata`.
+
+    The following ``kwargs`` are used to construct instances of
+    this type. Member attributes have the same names as these
+    ``kwargs```:
+
+    :param g: Genetic value
+    :type g: float
+    :param e: "Random/environmental" value
+    :type e: float
+    :param w: Fitness
+    :type w: float
+    :param sex: Sex
+    :type sex: int
+    :param deme: Deme
+    :type deme: int
+    :param label: Index in the population at time of living/sampling
+    :type label: int
+    :param alive: Individual is alive
+    :type alive: bool
+    :param preserved: Individual is preserved
+    :type preserved: bool
+    :param first_generation: Individual is first generation
+    :type first_generation: bool
+    :param parents: ``label`` of parents
+    :type parents: list
+    :param geography: Location in 3d space
+    :type geography: list
+    :param nodes: Nodes on tree sequence
+    :type nodes: list
+
+    .. versionadded:: 0.12.0
+    """
+
     g: float
     e: float
     w: float
     sex: int
     deme: int
     label: int
+    alive: bool
+    preserved: bool
+    first_generation: bool
     parents: typing.List[int]
     geography: typing.List[float]
     nodes: typing.List[int]
 
 
-def decode_individual_metadata(tc):
+def decode_individual_metadata(tc: tskit.TableCollection):
+    """
+    Decodes a :class:`tskit.IndividualTable`.
+
+    :param tc: A table collection
+    :type tc: :class:`tskit.TableCollection`
+
+    :returns: individual metadata
+    :rtype: List of :class:`fwdpy11.tskit_tools.DiploidMetadata`
+
+
+    .. versionadded:: 0.12.0
+    """
     rv = []
     for i in tc.individuals:
         alive = i.flags & fwdpy11.tskit_tools.INDIVIDUAL_IS_ALIVE
@@ -56,6 +107,17 @@ def decode_individual_metadata(tc):
 
 
 def decode_mutation_metadata(tc: tskit.TableCollection) -> fwdpy11.MutationVector:
+    """
+    Decodes a :class:`tskit.MutationTable`.
+
+    :param tc: A table collection
+    :type tc: :class:`tskit.TableCollection`
+
+    :returns: Mutations
+    :rtype: :class:`fwdpy11.MutationVector`
+
+    .. versionadded:: 0.12.0
+    """
     mutations = fwdpy11.MutationVector()
     for m in tc.mutations:
         md = m.metadata
