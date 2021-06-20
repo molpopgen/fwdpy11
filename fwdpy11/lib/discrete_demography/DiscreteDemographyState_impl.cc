e84520e6d90edda0a9816eb09aad7da71ad0a6d7
molpopgen <krthornt@uci.edu>
2021-06-19 21:49:27 -0700

    further split up implementation details

---
+++ fwdpy11/lib/discrete_demography/DiscreteDemographyState_impl.cc
@@ -0,0 +1,29 @@
+#include "DiscreteDemographyState_impl.hpp"
+
+namespace fwdpy11
+{
+    namespace discrete_demography
+    {
+        DiscreteDemographyState::DiscreteDemographyState_impl::
+            DiscreteDemographyState_impl(
+                std::vector<MassMigration> mass_migrations,
+                std::vector<SetExponentialGrowth> set_growth_rates,
+                std::vector<SetDemeSize> set_deme_sizes,
+                std::vector<SetSelfingRate> set_selfing_rates, MigrationMatrix M,
+                std::vector<SetMigrationRates> set_migration_rates)
+            : mass_migrations{std::move(mass_migrations)},
+              set_growth_rates{std::move(set_growth_rates)}, set_deme_sizes{std::move(
+                                                                 set_deme_sizes)},
+              set_selfing_rates{std::move(set_selfing_rates)}, M{std::move(M)},
+              set_migration_rates{std::move(set_migration_rates)}
+        {
+        }
+
+        std::unique_ptr<DiscreteDemographyState::DiscreteDemographyState_impl>
+        DiscreteDemographyState::DiscreteDemographyState_impl::clone() const
+        {
+            return std::make_unique<
+                DiscreteDemographyState::DiscreteDemographyState_impl>(*this);
+        }
+    }
+}
