5c294229d761f5aede94c5302eb9928d4672417b
molpopgen <krthornt@uci.edu>
2022-04-20 10:53:56 -0700

    Update Docker work flow for building wheels:
    * update location of requirements file
    * build wheels for Py 3.10

--- deployment/linux_wheels/build_and_audit.sh
+++ deployment/linux_wheels/build_and_audit.sh
@@ -1,11 +1,11 @@
 rm -rf dist/
 
-for py in cp37-cp37m cp38-cp38 cp39-cp39
+for py in cp37-cp37m cp38-cp38 cp39-cp39 cp310-cp310
 do
     rm -rf build
     PYPATH=/opt/python/${py}
     PYBIN=${PYPATH}/bin/python 
-    ${PYBIN} -m pip install --no-cache-dir -r requirements.txt
+    ${PYBIN} -m pip install --no-cache-dir -r requirements/development.txt
     PATH=${PYPATH}:$PATH ${PYBIN} setup.py build_ext -i
     PATH=${PYPATH}:$PATH ${PYBIN} setup.py bdist_wheel
 done
