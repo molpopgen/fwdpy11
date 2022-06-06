6435ca0350b9b77eb25288d9a54d24fb9b550a9e
molpopgen <krthornt@uci.edu>
2022-06-06 15:11:14 -0700

    add FIXME

--- tests/test_ForwardDemesGraph.py
+++ tests/test_ForwardDemesGraph.py
@@ -1,6 +1,8 @@
 import fwdpy11
 
 
+# FIXME: remove this test
+# when we have a working public API
 def test_basic_semantics():
     g = fwdpy11._fwdpy11._ll_ForwardDemesGraph()
     _ = g.add_deme("bananas", 0, 0)
