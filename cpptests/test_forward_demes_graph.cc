ec6afc16684e84cc261295a287435b671124deb2
molpopgen <krthornt@uci.edu>
2022-06-06 09:40:34 -0700

    check error condtion

--- cpptests/test_forward_demes_graph.cc
+++ cpptests/test_forward_demes_graph.cc
@@ -43,4 +43,20 @@ BOOST_AUTO_TEST_CASE(add_epoch_to_deme)
     assert(!g.demes[0].epochs.empty());
 }
 
+BOOST_AUTO_TEST_CASE(add_epoch_to_deme_with_bad_size_function)
+{
+    fwdpy11::discrete_demography::ForwardDemesGraph g;
+
+    auto deme = g.add_deme("CEU", 0, 0);
+
+    // end tie, start size, end size, cloning, selfing, size_function
+    BOOST_CHECK_THROW(
+        {
+            deme->add_epoch(100, 100, 150, 0.,
+                            fwdpy11::discrete_demography::Selfing::wright_fisher(),
+                            fwdpy11::discrete_demography::constant_size_function());
+        },
+        std::invalid_argument);
+}
+
 BOOST_AUTO_TEST_SUITE_END()
