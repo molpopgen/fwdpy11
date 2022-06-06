9fb11924452bfcee07f6f1a4dc1c506cd7c10dcb
molpopgen <krthornt@uci.edu>
2022-06-06 15:22:47 -0700

    establish a low-level Python pattern

--- fwdpy11/src/discrete_demography/ForwardDemesGraph.cc
+++ fwdpy11/src/discrete_demography/ForwardDemesGraph.cc
@@ -8,20 +8,25 @@ using namespace fwdpy11::discrete_demography;
 void
 init_ForwardDemesGraph(py::module& m)
 {
-    py::class_<::ForwardDemesGraph>(m, "_ll_ForwardDemesGraph")
+    py::class_<ForwardDemesGraph>(m, "_ll_ForwardDemesGraph")
         .def(py::init<>())
         .def("_add_deme", &::ForwardDemesGraph::add_deme);
 
-    py::class_<::Selfing>(m, "_ll_Selfing");
+    py::class_<Selfing>(m, "_ll_Selfing");
+    py::class_<SizeFunction>(m, "_ll_SizeFunction");
 
-    py::class_<::DemeRef>(m, "_ll_DemeRef")
-        .def("_add_epoch",
-             [](::DemeRef& self, demes_model_time end_time, std::uint32_t start_size,
-                std::uint32_t end_size, double cloning_rate, Selfing selfing,
-                SizeFunction size_function) {
-                 return self.deme.get().add_epoch(end_time, start_size, end_size,
-                                                  cloning_rate, selfing, size_function);
-             });
+    py::class_<DemeRef>(m, "_ll_DemeRef")
+        .def(
+            "_add_epoch",
+            [](DemeRef& self, demes_model_time end_time, std::uint32_t start_size,
+               std::uint32_t end_size, double cloning_rate, Selfing selfing,
+               SizeFunction size_function) {
+                return self.deme.get().add_epoch(end_time, start_size, end_size,
+                                                 cloning_rate, selfing, size_function);
+            },
+            py::kw_only(), py::arg("end_time"), py::arg("start_size"),
+            py::arg("end_size"), py::arg("cloning_rate"), py::arg("selfing"),
+            py::arg("size_function"));
 
     m.def("_constant_size_function", &constant_size_function);
 
