f76478463e598254e85705effda4deeabe31d6cd
molpopgen <krthornt@uci.edu>
2022-06-06 07:18:14 -0700

    tests pass

--- cpptests/test_forward_demes_graph.cc
+++ cpptests/test_forward_demes_graph.cc
@@ -22,16 +22,23 @@ BOOST_AUTO_TEST_CASE(add_deme_that_already_exists)
     BOOST_CHECK_THROW({ g.add_deme("CEU", 0, 0); }, std::invalid_argument);
 }
 
-BOOST_AUTO_TEST_CASE(add_epoch_to_deme)
+BOOST_AUTO_TEST_CASE(constant_size_function)
 {
-    fwdpy11::discrete_demography::ForwardDemesGraph g;
-
-    auto deme = g.add_deme("CEU", 0, 0);
-
-    // end tie, start size, end size, cloning, selfing, size_function
-    deme.add_epoch(100, 150, 100, 0.,
-                   fwdpy11::discrete_demography::Selfing::wright_fisher(),
-                   fwdpy11::discrete_demography::SizeFunction::constant());
+    auto e = fwdpy11::discrete_demography::SizeFunction::constant();
+    e.validate(10, 10);
+    BOOST_CHECK_THROW({ e.validate(100, 200); }, std::invalid_argument);
 }
+
+//BOOST_AUTO_TEST_CASE(add_epoch_to_deme)
+//{
+//    fwdpy11::discrete_demography::ForwardDemesGraph g;
+//
+//    auto deme = g.add_deme("CEU", 0, 0);
+//
+//    // end tie, start size, end size, cloning, selfing, size_function
+//    deme.add_epoch(100, 150, 100, 0.,
+//                   fwdpy11::discrete_demography::Selfing::wright_fisher(),
+//                   fwdpy11::discrete_demography::SizeFunction::constant());
+//}
 
 BOOST_AUTO_TEST_SUITE_END()
