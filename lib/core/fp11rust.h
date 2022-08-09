899e7e0de9262a8d53d017806eace4427fb3e43f
Kevin R. Thornton <krthornt@uci.edu>
2022-08-09 12:27:16 -0700

    Remove cbindgen warning: (#965)
    * add cbindgen.toml
    * specify dependencies to parse

--- lib/core/fp11rust.h
+++ lib/core/fp11rust.h
@@ -2,12 +2,6 @@
 extern "C" {
 #endif // __cplusplus
 
-/*
- * Required to make our bridge to demes-forward-capi 
- * know that this type will be provided at link-time
- */
-typedef struct OpaqueForwardGraph OpaqueForwardGraph;
-
 #include "internal/fp11rust_api.h"
 
 #ifdef __cplusplus
