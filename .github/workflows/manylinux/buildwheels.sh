969979728992447b4e2cb2b658b0f9a17dc69fd3
Kevin R. Thornton <krthornt@uci.edu>
2022-10-06 09:38:55 -0700

    Change wheel builds to be based on manylinux_2_28
    Closes #992

--- .github/workflows/manylinux/buildwheels.sh
+++ .github/workflows/manylinux/buildwheels.sh
@@ -3,7 +3,7 @@
 set -e -x
 
 yum update -y
-yum -y install cmake curl
+yum -y install cmake curl gsl-devel
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | bash -s -- -y 
 source "$HOME/.cargo/env" 
@@ -15,14 +15,14 @@ rustup override set 1.62.1
 # Pin cbindgen 
 cargo install cbindgen@0.24.3 
 
-GSL_VERSION=2.5
-curl -o gsl-${GSL_VERSION}.tar.gz "ftp://ftp.gnu.org/gnu/gsl/gsl-${GSL_VERSION}.tar.gz" 
-tar -zxf gsl-${GSL_VERSION}.tar.gz 
-cd gsl-${GSL_VERSION} 
-./configure --prefix=/usr/local
-make -j 2 
-make install 
-cd .. 
+# GSL_VERSION=2.5
+# curl -o gsl-${GSL_VERSION}.tar.gz "ftp://ftp.gnu.org/gnu/gsl/gsl-${GSL_VERSION}.tar.gz" 
+# tar -zxf gsl-${GSL_VERSION}.tar.gz 
+# cd gsl-${GSL_VERSION} 
+# ./configure --prefix=/usr/local
+# make -j 2 
+# make install 
+# cd .. 
 
 # Taken from msprime/#2043
 # We're running as root in the docker container so git commands issued by
